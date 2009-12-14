820f3d864508e0b42dbb9c9c94e663acd25e34cc
Matt Zwier <mzwier2@uiuc.edu>
2009-12-14 09:59:25 -0500

    Updated ttimes.py for efficiency and sanity

--- src/ttimes.py
+++ src/ttimes.py
@@ -1,157 +1,117 @@
-from __future__ import division
-import os, sys, re, math
-from itertools import izip
+import os, sys
 from optparse import OptionParser
-import cPickle as pickle
+from configparse import SafeConfigParser
+import numpy
+from wemd.util.binfiles import UniformTimeData
+from itertools import izip
 
-class ArraySaver(object):
-    text_cell_format = '%-21.17g'
-    pickle_protocol = pickle.HIGHEST_PROTOCOL
-    default_file_formats = ('text', 'pickle')
-    format_exts = {'text': '.txt',
-                   'pickle': '.dat'}
-    format_fileformats = {'text': 't',
-                          'pickle': 'b'}
-    
-    def __init__(self, file_formats = None,
-                 ignore_errors = False):
-        self.file_formats = file_formats or self.default_file_formats
-        self.ignore_errors = ignore_errors
-        
-    def save_array(self, data, filename_template, template_params, 
-                   file_formats = None, ignore_errors = False):
+#region_edges = [0.0, 0.4, 0.65, 0.85, 1.0, float('inf')]
+#region_names = ['B', 'T3', 'T2', 'T1', 'A']
+region_edges = [0.0, 0.4, 1.0, float('inf')]
+region_names = ['B', 'T', 'A']
 
-        file_formats = file_formats or self.file_formats
-        tparams = dict(template_params.iteritems())
-        
-        for fmt in self.file_formats:
-            tparams['file_format'] = fmt
-            saver = getattr(self, 'save_fmt_%s' % fmt)
-            try:
-                tparams['format_ext'] = self.format_exts[fmt]
-            except IndexError:
-                pass
-            filename = filename_template % tparams
-            
-            try:
-                stream = open(filename, 'w' + self.format_fileformats[fmt])
-            except OSError, e:
-                if ignore_errors:
-                    continue
-                else:
-                    raise
-            else:
-                saver(data, stream)
-                stream.close()
-        
-    def save_fmt_text(self, data, stream):
-        if len(data.shape) > 2:
-            raise TypeError('cannot save array of rank > 2 as text')
-        elif len(data.shape) == 0:
-            stream.write((self.text_cell_format % data) + '\n')
-            return
-        elif len(data.shape) == 1:
-            for row in data:
-                stream.write((self.text_cell_format % row) + '\n')
-            return
-        else:
-            cols = data.shape[1]
-            rowfmt = ' '.join([self.text_cell_format] * cols) + '\n'
-            for row in data:
-                stream.write(rowfmt % tuple(row))
-            return
-        
-    def save_fmt_pickle(self, data, stream):
-        pickle.dump(data, stream, self.pickle_protocol)
-        
-        
+tb_by_type = {(0,2): [], (2,0): []}
+fpt_by_type = {(0,2): [], (2,0): []}
 
-def write_array_as_text(data, stream, format='-15.8g'):
-    if len(data.shape) > 2:
-        raise TypeError('cannot save array of rank > 2 as text')
-    rows = data.shape[0]
-    cols = data.shape[1]
-    
-    rowfmt = ' '.join([format] * cols) + '\n'
-    
-    for row in data:
-        stream.write(rowfmt % row)
+assert len(region_edges) == len(region_names) + 1
+nreg = len(region_names)
 
-parser = OptionParser(usage = '%prog PC_ARRAY TRANS_LB TRANS_UB')
-(opts, args) = parser.parse_args()
+dist_utd = UniformTimeData()
+dist = dist_utd.mmap_array_from(open('dist.dat', 'rb'))
+dt = dist_utd.dt
 
-if len(args) != 3:
-    parser.print_help(sys.stderr)
-    sys.exit(2)
-    
-import numpy, wemd
+#td = numpy.load('dist.pkl')
+#dist = numpy.zeros(td.shape, numpy.float)
+#dist[:,0] = td[:,1] * 0.1
+#dist[:,1] += 1.0
+#dt = td[1,0] - td[0,0]
+#print dist[0:4]
 
-dat = numpy.load(args[0])
-if len(dat.shape) < 2:
-    Q = dat
-    t = numpy.fromiter(xrange(0,dat.shape[0]), numpy.float_)
-    sys.stderr.write('Warning: no time values available\n')
+completion_times = numpy.zeros((nreg,nreg), numpy.int64) - 1
+event_count = numpy.zeros((nreg,nreg), numpy.int64)
+
+for ir in xrange(0, nreg):
+    q = dist[0,0]
+    if region_edges[ir] <= dist[0,0] < region_edges[ir+1]:
+        last_iregion = ir
+        break
 else:
-    t = dat[:,0]
-    Q = dat[:,1]
-assert len(Q) == len(t)
-
-Q_trans_lb = float(args[1])
-Q_trans_ub = float(args[2])
-
-te_finder = wemd.analysis.transitions.PC1DCrossingFinder(Q_trans_lb, Q_trans_ub)
-te_finder.assign_regions(Q)
-te_finder.find_crossings()
-te_finder.find_transitions()
-
-asaver = ArraySaver()
-
-for (region1, region2) in sorted(te_finder.transition_indices):
-    filename_params = {'region1': region1,
-                       'region2': region2}
-    ti_array = te_finder.transition_indices[region1, region2]
-    sys.stdout.write('\n%s->%s Transitions\n' % (region1, region2))
-    trans_times = t[ti_array[:,1]] - t[ti_array[:,0]]
-    sys.stdout.write('Number of transition events: %d\n' % len(trans_times))
-    sys.stdout.write('Minimum event duration:      %g\n' % min(trans_times))
-    sys.stdout.write('Maximum event duration:      %g\n' % max(trans_times))
-    tt_mean = trans_times.mean()
-    se_tt_mean = trans_times.std() / (len(trans_times) ** 0.5)
-    sys.stdout.write('Mean event duration:         %g\n' % tt_mean)
-    sys.stdout.write('Std. err. of mean duration:  %g\n' % se_tt_mean)
-    asaver.save_array(trans_times, '%(region1)s%(region2)s_trans%(format_ext)s',
-                      filename_params)
-    quantiles = [0.01*x for x in xrange(1,100)]
-    tt_qs = numpy.empty((len(quantiles), 2), numpy.float_)
-    tt_qs[:,0] = quantiles
-    tt_qs[:,1] = wemd.analysis.probability.quantiles(trans_times, quantiles)
-    asaver.save_array(tt_qs,
-                      '%(region1)s%(region2)s_trans_quantiles%(format_ext)s',
-                      filename_params)
+    raise ValueError('particle is outside specified coordinate space')
     
-    try:
-        pi_array = te_finder.passage_indices[region1, region2]
-    except KeyError:
-        pass
-    else:
-        passage_times = t[pi_array[:,1]] - t[pi_array[:,0]]
-        mfpt = passage_times.mean()
-        se_mfpt = passage_times.std() / (len(passage_times) ** 0.5)
-        k = 1.0/mfpt
-        se_k = 1.0/mfpt**2 * se_mfpt
-        sys.stdout.write('Minimum first passage time:  %g\n' % min(passage_times))
-        sys.stdout.write('Maximum first passage time:  %g\n' % max(passage_times))
-        sys.stdout.write('Mean first passage time:     %g\n' % mfpt)
-        sys.stdout.write('StdErr of MFPT:              %g\n' % se_mfpt)
-        sys.stdout.write('Reaction rate:               %g\n' % k)
-        sys.stdout.write('StdErr of reation rate:      %g\n' % se_k)
-        asaver.save_array(passage_times, 
-                          '%(region1)s%(region2)s_passage%(format_ext)s',
-                          filename_params)
-        pt_qs = numpy.empty((len(quantiles), 2), numpy.float_)
-        pt_qs[:,0] = quantiles
-        pt_qs[:,1] = wemd.analysis.probability.quantiles(passage_times, quantiles)
-        asaver.save_array(pt_qs, 
-                          '%(region1)s%(region2)s_passage_quantiles%(format_ext)s',
-                          filename_params)
+for it in xrange(1, dist.shape[0]):
+    q = dist[it,0]
+    for ir in xrange(0, nreg):
+        lb = region_edges[ir]
+        ub = region_edges[ir+1]
         
+        if lb <= q < ub:
+            iregion = ir
+            break
+    else:
+        raise ValueError('particle is outside specified coordinate space')
+    
+    if iregion != last_iregion:        
+        # Crossing event...do all the real work here
+    
+        completion_times[last_iregion, iregion] = it
+        event_count[last_iregion, iregion] += 1
+                
+        #print "%d (%s->%s)" % (it, region_names[last_iregion], region_names[iregion])
+        
+        for irr in xrange(1, nreg):
+            if iregion > last_iregion:
+                trans_iregion = last_iregion - irr
+                tb_iregion = trans_iregion + 1
+            else:
+                trans_iregion = last_iregion + irr
+                tb_iregion = trans_iregion - 1
+                
+            if 0 <= trans_iregion < nreg and trans_iregion != iregion:
+                if completion_times[iregion, last_iregion] > completion_times[trans_iregion, last_iregion]:
+                    # Fluctuation between regions
+                    #print "  cannot be a %s->%s transition" % (region_names[trans_iregion], region_names[iregion])
+                    pass
+                else:
+                    try:
+                        type_tbs = tb_by_type[trans_iregion, iregion]
+                        type_fpts = fpt_by_type[trans_iregion, iregion]
+                    except KeyError:
+                        pass
+                    else:
+                        #print "  %s->%s transition complete" % (region_names[trans_iregion], region_names[iregion])
+                
+                        # First passage time:  time since last transition ending in starting state
+                        if completion_times[iregion, trans_iregion] >= 0:
+                            fpt = it-completion_times[iregion, trans_iregion]
+                            type_fpts.append(fpt) 
+                            #print "  %s->%s first passage time: %d" \
+                            #      % (region_names[trans_iregion], region_names[iregion], it-completion_times[iregion, trans_iregion])
+                    
+                        # Event duration: time since last transition originating from starting state
+                        # trans_iregion -> trans_iregion +/- 1
+                        if completion_times[trans_iregion, tb_iregion] >= 0:
+                            tb = it-completion_times[trans_iregion, tb_iregion]
+                            type_tbs.append(tb)
+                            #print "  %s->%s event duration: %d" \
+                            #      % (region_names[trans_iregion], region_names[iregion], 
+                            #         it-completion_times[trans_iregion, tb_iregion])
+                    # Update completion times matrix
+                    completion_times[trans_iregion, iregion] = it
+                    event_count[trans_iregion, iregion] += 1
+        last_iregion = iregion
+    #if it == 1000000: break
+del dist
+print "event counts:"
+print event_count
+
+tb_AB = numpy.array(tb_by_type[2,0], numpy.float)
+tb_AB = tb_AB * dt
+fpt_AB = numpy.array(fpt_by_type[2,0], numpy.float)
+fpt_AB = fpt_AB * dt
+print "average A->B duration:", tb_AB.mean()
+print "stdev   A->B duration:", tb_AB.std()
+print "SEM     A->B duration:", tb_AB.std()/(tb_AB.size**0.5)
+print
+print "average A->B FPT:", fpt_AB.mean()
+print "stdev   A->B FPT:", fpt_AB.std()
+print "SEM     A->B FPT:", fpt_AB.std()/(fpt_AB.size**0.5)
