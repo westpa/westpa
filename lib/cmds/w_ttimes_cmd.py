671a57b51529acd7d60afd2568a5cb28820cecac
Matt Zwier <mczwier@gmail.com>
2011-03-02 23:04:22 -0500

    Greatly increased transition time analysis throughput by copious numpy vectorization.

--- lib/cmds/w_ttimes_cmd.py
+++ lib/cmds/w_ttimes_cmd.py
@@ -53,6 +53,11 @@ class TransitionEventAccumulator:
         self.fptfile = fptfile
         self.ltfile = ltfile
         
+        self.track_transitions = (tfile is not None)
+        self.track_lifetimes = (ltfile is not None)
+        self.track_fpts = (fptfile is not None)
+        self.track_eds = (edfile is not None)
+        
         self.n_bins = len(self.region_set.get_all_bins())
         self.clear()
         
@@ -85,18 +90,41 @@ class TransitionEventAccumulator:
                              .format(time_index=long(time_index), label=label, weight=float(weight)))
     
     def record_lifetime_entry(self, time_index, final_region, lifetime, weight):
+        #self.lt_acc.incorporate(final_region, lifetime, weight)
+            #self.count[index] += 1
+            #self.weight[index] += weight
+            #self.sum[index] += value
+            #self.sqsum[index] += value*value
+        lt_acc = self.lt_acc
+        lt_acc.count[final_region] += 1
+        lt_acc.weight[final_region] += weight
+        lt_acc.sum[final_region] += lifetime
+        lt_acc.sqsum[final_region] += lifetime*lifetime
+        
         if self.ltfile:
             self.ltfile.write('{time_index:20d}    {label:10d}    {lifetime:20d}    {weight:20.14e}\n'
                               .format(time_index=long(time_index), label=long(final_region), lifetime=long(lifetime), 
                                       weight=float(weight)))
     
     def record_ed_entry(self, time_index, initial_region, final_region, ed, weight):
+        #self.ed_acc.incorporate((initial_region,final_region), ed, weight)
+        ed_acc = self.ed_acc
+        ed_acc.count[initial_region,final_region] += 1
+        ed_acc.weight[initial_region,final_region] += weight
+        ed_acc.sum[initial_region,final_region] += ed
+        ed_acc.sqsum[initial_region,final_region] += ed*ed
         if self.edfile:
             label = '{:d}->{:d}'.format(int(initial_region),int(final_region))
             self.edfile.write('{time_index:20d}    {label:<24s}    {ed:20d}    {weight:20.14e}\n'
                               .format(time_index=long(time_index), label=label, ed=long(ed), weight=float(weight)))
     
     def record_fpt_entry(self, time_index, initial_region, final_region, fpt, weight):
+        #self.fpt_acc.incorporate((initial_region,final_region), fpt, weight)
+        fpt_acc = self.fpt_acc
+        fpt_acc.count[initial_region,final_region] += 1
+        fpt_acc.weight[initial_region,final_region] += weight
+        fpt_acc.sum[initial_region,final_region] += fpt
+        fpt_acc.sqsum[initial_region,final_region] += fpt*fpt
         if self.fptfile:
             label = '{:d}->{:d}'.format(int(initial_region),int(final_region))
             self.fptfile.write('{time_index:20d}    {label:<24s}    {fpt:20d}    {weight:20.14e}\n'
@@ -118,75 +146,87 @@ class TransitionEventAccumulator:
         
         indices = self.region_set.map_to_indices(pcoords)
         
-        if not continuation:
-            self.clear() # clear out notions of when events last happened
-            self.time_index = time_index = time_index or 0 # set our "clock" for timing events
-            last_region = self.last_region = indices[0] # use the first point only to determine where we start
-            initial_index = 1
+        # a table of time points, region at that time point, and (yes, believe it), the region at the prior time point
+        # in that order
+        TIMEPOINT = 0; CURRENT_REGION = 1; LAST_REGION = 2
+        
+        if continuation:
+            tracking_table = numpy.empty((len(pcoords), 3), self.index_dtype)
+            tracking_table[0,LAST_REGION] = self.last_region
+            tracking_table[:,CURRENT_REGION] = indices
+            if len(pcoords) > 1:
+                tracking_table[1:,LAST_REGION] = indices[:-1]
+            tracking_table[:,TIMEPOINT] = numpy.arange(self.time_index+1, self.time_index+len(pcoords)+1, dtype=self.index_dtype)
         else:
-            # continuation
-            initial_index = 0  # use the first point
-            time_index = self.time_index # and continue counting time where we left off
-            last_region = self.last_region
+            # not a continuation
+            self.clear()
+            self.time_index = time_index = time_index or 0
+            tracking_table = numpy.empty((len(pcoords)-1,3), self.index_dtype)
+            tracking_table[:,LAST_REGION] = indices[:-1]
+            tracking_table[:,CURRENT_REGION] = indices[1:]
+            tracking_table[:,TIMEPOINT] = numpy.arange(self.time_index+1, self.time_index+len(pcoords), dtype=self.index_dtype)
+        
+        observed_at = tracking_table[:,CURRENT_REGION] != tracking_table[:,LAST_REGION]
+        trans_observed = tracking_table[observed_at, :]
+        weights_observed = weights[observed_at]
 
-        # pull all references to self out of the inner loop
+        # pull all references to self out of the inner loops for a 25% reduction in CPU time!
         last_crossing = self.last_crossing
         last_entry = self.last_entry
         last_exit = self.last_exit
         last_completion = self.last_completion
         n_crossings = self.n_crossings
         n_completions = self.n_completions
-        lt_acc_incorporate = self.lt_acc.incorporate
-        ed_acc_incorporate = self.ed_acc.incorporate
-        fpt_acc_incorporate = self.fpt_acc.incorporate
         record_transition_entry = self.record_transition_entry
         record_lifetime_entry = self.record_lifetime_entry
         record_ed_entry = self.record_ed_entry
         record_fpt_entry = self.record_fpt_entry
         n_bins = self.n_bins
+        index_dtype = self.index_dtype
+        track_transitions = self.track_transitions
+        track_lifetimes = self.track_lifetimes
+        track_eds = self.track_eds
+        track_fpts = self.track_fpts
+
+        irdisc = numpy.zeros((n_bins,), numpy.bool_)        
+        init_regions = numpy.arange(0,n_bins,dtype=index_dtype)
         
-        for pcidx in xrange(initial_index, len(pcoords)):
-            current_region = indices[pcidx]
-            weight = weights[pcidx]
+        for ((time_index,current_region,last_region), weight) in izip(trans_observed, weights_observed):
+            n_crossings[last_region,current_region] += 1
+            if track_transitions:
+                record_transition_entry(time_index,last_region,current_region,weight)
             
-            if current_region != last_region:
-                n_crossings[last_region,current_region] += 1
-                record_transition_entry(time_index, last_region, current_region, weight)
-            
-                if last_entry[last_region] > 0:
-                    lifetime = time_index - last_entry[last_region]
-                    lt_acc_incorporate(last_region, lifetime, weight)
-                    record_lifetime_entry(time_index, last_region, lifetime, weight)
-                    
-                for initial_region in xrange(0, n_bins):
-                    if initial_region == last_region or initial_region == current_region: continue
-                    
-                    if last_entry[initial_region] > last_completion[initial_region, current_region]:
-                        record_transition_entry(time_index, initial_region, current_region, weight)
-                        
-                        ed = time_index - last_exit[initial_region]
-                        ed_acc_incorporate((initial_region,current_region), ed, weight)
-                        record_ed_entry(time_index, initial_region, current_region, ed, weight)
-                        
-                        # FPTs can be computed for initial->current if current->initial has been seen
-                        if last_completion[current_region, initial_region] > 0:
-                            fpt = time_index - last_completion[current_region, initial_region]
-                            fpt_acc_incorporate((initial_region,current_region), fpt, weight)
-                            record_fpt_entry(time_index, initial_region, current_region, fpt, weight)
-                        
-                        last_completion[initial_region,current_region] = time_index
-                        n_completions[initial_region,current_region] += 1
-                        
-                last_exit[last_region] = time_index
-                last_entry[current_region] = time_index
-                last_crossing[last_region,current_region] = time_index
+            if track_lifetimes and last_entry[last_region] > 0:
+                lifetime = time_index - last_entry[last_region]
+                record_lifetime_entry(time_index, last_region, lifetime, weight)
                 
-            time_index += 1
-            last_region = current_region
-        else:
-            # store information for next invocation
-            self.time_index = time_index
-            self.last_region = current_region
+            irdisc[:] = True
+            irdisc[last_region] = False
+            irdisc[current_region] = False
+            irdisc &= (last_entry > last_completion[:,current_region])
+            
+            for initial_region in init_regions[irdisc]:
+                
+                if track_transitions:
+                    record_transition_entry(time_index,initial_region,current_region,weight)
+                if track_eds:
+                    ed = time_index - last_exit[initial_region]
+                    record_ed_entry(time_index,initial_region,current_region,ed,weight)
+                
+                if track_fpts and last_completion[current_region,initial_region] > 0:
+                        fpt = time_index - last_completion[current_region,initial_region]
+                        record_fpt_entry(time_index,initial_region,current_region,fpt,weight)
+                
+                last_completion[initial_region,current_region] = time_index
+                n_completions[initial_region,current_region] += 1
+                         
+            last_exit[last_region] = time_index
+            last_entry[current_region] = time_index
+            last_crossing[last_region,current_region] = time_index
+                    
+        self.time_index = tracking_table[-1,TIMEPOINT]
+        self.last_region = tracking_table[-1,CURRENT_REGION]
+
             
 def report_counts_averages(transacc, output_file):
     print('Number of crossings:',file=output_file)
@@ -194,20 +234,23 @@ def report_counts_averages(transacc, output_file):
     print('Number of completed transitions:',file=output_file)
     print(transacc.n_completions,file=output_file)
     
-    print('Average lifetimes:',file=output_file)
-    print(transacc.lt_acc.average(),file=output_file)
-    print('Standard deviation of dwell times:',file=output_file)
-    print(transacc.lt_acc.std(),file=output_file)
+    if transacc.track_lifetimes:
+        print('Average lifetimes:',file=output_file)
+        print(transacc.lt_acc.average(),file=output_file)
+        print('Standard deviation of dwell times:',file=output_file)
+        print(transacc.lt_acc.std(),file=output_file)
     
-    print('Average event durations:',file=output_file)
-    print(transacc.ed_acc.average(),file=output_file)
-    print('Standard deviation of event durations:',file=output_file)
-    print(transacc.ed_acc.std(),file=output_file)
-    
-    print('Average first passage times:',file=output_file)
-    print(transacc.fpt_acc.average(),file=output_file)
-    print('Standard deviation of first passage times:',file=output_file)
-    print(transacc.fpt_acc.std(),file=output_file)
+    if transacc.track_eds:
+        print('Average event durations:',file=output_file)
+        print(transacc.ed_acc.average(),file=output_file)
+        print('Standard deviation of event durations:',file=output_file)
+        print(transacc.ed_acc.std(),file=output_file)
+
+    if transacc.track_fpts:    
+        print('Average first passage times:',file=output_file)
+        print(transacc.fpt_acc.average(),file=output_file)
+        print('Standard deviation of first passage times:',file=output_file)
+        print(transacc.fpt_acc.std(),file=output_file)
     
     
 def run_transanl_bf(args, transacc, pcoords):
@@ -232,24 +275,20 @@ def run_transanl_bf(args, transacc, pcoords):
         else:
             transacc.accumulate_transitions(pc_chunk, continuation=True)
             
-        if args.verbose_mode:
-            transacc.rtracker.dump_differences(args.output_file)
-
-
 parser = wemd.rc.common_arg_parser('w_ttimes', description='''
 Perform lifetime and transition analysis on WEMD or brute force data.''')
 parser.add_argument('-o', '--output', dest='output_file',
                     help='Store output in OUTPUT_FILE (default: write to standard output).',
                     type=argparse.FileType('wt'), default=sys.stdout)
 #parser.add_argument('-I', '--save-converted-input', )
-parser.add_argument('-T', '--transitions', dest='trans_file', type=argparse.FileType('wt'), default='transitions.txt',
-                    help='List transitions to TRANS_FILE (default: transitions.txt)')
+parser.add_argument('-T', '--transitions', dest='trans_file', type=argparse.FileType('wt'),
+                    help='List transitions to TRANS_FILE (default: do not list)')
 parser.add_argument('-E', '--eds', dest='ed_file', type=argparse.FileType('wt'), default='eds.txt',
                     help='List event durations to ED_FILE (default: durations.txt)')
-parser.add_argument('-F', '--fpts', dest='fpt_file', type=argparse.FileType('wt'), default='fpts.txt',
-                    help='List first passage times to FPT_FILE (default: fpts.txt)')
-parser.add_argument('-L', '--lifetimes', dest='lifetime_file', type=argparse.FileType('wt'), default='lifetimes.txt',
-                    help='List times spent in each region to LIFETIME_FILE (default: lifetimes.txt)')
+parser.add_argument('-F', '--fpts', dest='fpt_file', type=argparse.FileType('wt'),
+                    help='List first passage times to FPT_FILE (default: not analyzed)')
+parser.add_argument('-L', '--lifetimes', dest='lifetime_file', type=argparse.FileType('wt'),
+                    help='List times spent in each region to LIFETIME_FILE (default: do not list)')
 parser.add_argument('--noheaders', dest='suppress_headers', action='store_true',
                     help='Do not write headers to output files (default: write headers).')
 parser.add_argument('-b', '--bins', dest='binexpr',
@@ -294,27 +333,31 @@ else:
     region_set = sim_manager.system.region_set
 
 if not args.suppress_headers:
-    args.trans_file.write('''\
+    if args.trans_file is not None:
+        args.trans_file.write('''\
 # Crossings
 # column 0: time point of end of transition
 # column 1: initial->final bin indices
 # column 2: weight at end of transition 
 ''')
-    args.lifetime_file.write('''\
+    if args.lifetime_file is not None:
+        args.lifetime_file.write('''\
 # Lifetimes
 # column 0: time point
 # column 1: bin index
 # column 2: dwell time
 # column 3: weight at end of transition
 ''')
-    args.ed_file.write('''\
+    if args.ed_file is not None:
+        args.ed_file.write('''\
 # Event durations
 # column 0: time point of end of transition
 # column 1: initial->final bin indices
 # column 2: event duration
 # column 3: weight at end of transition
 ''')
-    args.fpt_file.write('''\
+    if args.fpt_file is not None:
+        args.fpt_file.write('''\
 # First passage times
 # column 0: time point of end of transition
 # column 1: initial->final bin indices
