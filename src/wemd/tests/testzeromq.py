b93e53cb73d767d4d8db5a8cc7f9d691d7d7fa2d
Matt Zwier <mczwier@gmail.com>
2011-05-06 14:11:27 -0400

    ZMQ client and server widgets can round-trip work

--- src/wemd/tests/testzeromq.py
+++ src/wemd/tests/testzeromq.py
@@ -5,54 +5,64 @@ import wemd.work_managers.zeromq
 import nose.tools
 import zmq
 
-from wemd.work_managers.zeromq import ZWorkProvider
+from wemd.work_managers.zeromq import ZWorkProvider, ZWorkConsumer
 
 
 TIME_FAST = 0.5
-TIME_MEDIUM = 4*TIME_FAST
-TIME_SLOW = 5*TIME_MEDIUM
+TIME_MEDIUM = 2
+TIME_SLOW = 10
 
-DELAY_FAST = TIME_FAST/10.0
-DELAY_MEDIUM = TIME_MEDIUM/10.0
-DELAY_SLOW = TIME_SLOW/10.0
+DELAY_FAST = 0.05
+DELAY_MEDIUM = 0.2
+DELAY_SLOW = 1
 
 
 class TestZWorkProvider:
     def __init__(self):
         self.context = zmq.Context()
         
-        self.task_endpoint = 'inproc://tasks'
-        self.results_endpoint = 'inproc://results'
-        self.ann_endpoint = 'inproc://announce'
-        self.ann_socket = self.context.socket(zmq.SUB)
-        self.ann_socket.setsockopt(zmq.SUBSCRIBE,'')
-        self.ann_socket.bind(self.ann_endpoint)
+        #self.task_endpoint = 'inproc://tasks'
+        #self.results_endpoint = 'inproc://results'
+        #self.ann_endpoint = 'inproc://announce'
         
+        #self.task_endpoint = 'ipc:///tmp/tasks'
+        #self.results_endpoint = 'ipc:///tmp/results'
+        #self.ann_endpoint = 'ipc:///tmp/announce'
+        
+        self.task_endpoint = 'tcp://127.0.0.1:23811'
+        self.results_endpoint = 'tcp://127.0.0.1:23812'
+        self.ann_endpoint = 'tcp://127.0.0.1:23813'
+                
     def make_provider(self):
         zwp = ZWorkProvider(self.context, self.task_endpoint, self.results_endpoint, self.ann_endpoint)
+        self.ann_socket = self.context.socket(zmq.SUB)
+        self.ann_socket.setsockopt(zmq.SUBSCRIBE,'')
+        self.ann_socket.setsockopt(zmq.LINGER,0)
+        self.ann_socket.connect(self.ann_endpoint)
+        time.sleep(DELAY_FAST)
         return zwp
             
     @nose.tools.timed(TIME_FAST)
-    def test_announce(self):        
+    def test_announce(self):    
         zwp = self.make_provider()
         zwp.announce_work()
         rsl = self.ann_socket.recv()
         assert rsl.startswith('tasks available')
+        
+        # A delay after shutdown appears necessary before running
+        # other tests
+        zwp.shutdown()
+        time.sleep(DELAY_FAST)
 
         
-    @nose.tools.timed(TIME_FAST)
+    @nose.tools.timed(TIME_MEDIUM)
     def test_dispatch(self):
         zwp = self.make_provider()
         
         task_socket = self.context.socket(zmq.REQ)
         task_socket.connect(self.task_endpoint)
-        
-        #zwp.outgoing_tasks.extend([1,'2', 3.0])
 
-        #t = threading.Thread(target=zwp.dispatch_loop)
-        #t.start()
         zwp.dispatch([1,'2', 3.0])
-        rsl = self.ann_socket.recv()
         
         task_socket.send('request 2')
         rsl = task_socket.recv_multipart()
@@ -71,8 +81,10 @@ class TestZWorkProvider:
         rsl = task_socket.recv_multipart()
         assert rsl[0] == 'tasks 0'
         assert rsl[1] == ''
-        zwp.shutdown()
         
+        zwp.shutdown()
+        time.sleep(DELAY_FAST)
+                
     @nose.tools.timed(TIME_FAST)
     def test_collect(self):
         zwp = self.make_provider()
@@ -80,10 +92,51 @@ class TestZWorkProvider:
         
         results_socket = self.context.socket(zmq.PUSH)
         results_socket.connect(self.results_endpoint)
-        
-        results_socket.send('propagate', zmq.SNDMORE)
         results_socket.send_pyobj([1, '2', 3.0])
         time.sleep(DELAY_FAST)
         assert list(zwp.incoming_results) == [1, '2', 3.0]
+
         zwp.shutdown()
+        time.sleep(DELAY_FAST)
+
+class TestZWorkConsumer:
+    def __init__(self):
+        self.context = zmq.Context()
         
+        self.task_endpoint = 'inproc://tasks'
+        self.results_endpoint = 'inproc://results'
+        self.ann_endpoint = 'inproc://announce'
+        
+    def make_provider(self):
+        zwp = ZWorkProvider(self.context, self.task_endpoint, self.results_endpoint, self.ann_endpoint)
+        time.sleep(DELAY_FAST)
+        return zwp
+    
+    def make_consumer(self, blocksize, propagator):
+        zwc = ZWorkConsumer(self.context, self.ann_endpoint, blocksize, propagator)
+        time.sleep(DELAY_FAST)
+        return zwc
+    
+    def test_work(self):
+        class Propagator:
+            def propagate(self, segments):
+                for i in xrange(0, len(segments)):
+                    segment = segments[i]
+                    segment = [pcoord + 1.0 for pcoord in segment]
+                    segments[i] = segment
+        
+        zwp = self.make_provider()
+        zwp.dispatch([('propagate', [1.0,2.0,3.0-1j]), ('propagate', [5])])
+        zwp.collect()
+        
+        zwc = self.make_consumer(2, Propagator())
+        zwc.work()
+        nloops = 0
+        while nloops < 10 and len(zwp.incoming_results) == 0:
+            time.sleep(DELAY_FAST)
+        assert nloops != 10    
+        assert list(zwp.incoming_results) == [('propagate', [2.0, 3.0, 4.0-1j]),
+                                              ('propagate', [6])]
+        
+        zwc.shutdown()
+        zwp.shutdown()            
--- src/wemd/work_managers/zeromq.py
+++ src/wemd/work_managers/zeromq.py
@@ -13,18 +13,12 @@ class ZMQBase:
         self.nodespec = '{}#{:d}:0x{:x}'.format(self.hostname, os.getpid(), threading.current_thread().ident)
         self.node_id = str(uuid.uuid4())        
         
-        # An inproc PUB socket for asynchronous inter-thread signaling
-        self._intsig_endpoint = None
-        self._intsig = None
-        
-    def bind_intsig(self):
-        self._intsig_endpoint = 'inproc://_intsig'
+        # An inproc PUB socket for asynchronous inter-thread signaling WITHIN THIS CLASS
+        self._intsig_endpoint = 'inproc://_intsig_{:x}'.format(id(self))
         self._intsig = self.context.socket(zmq.PUB)
         self._intsig.bind(self._intsig_endpoint)
-        
+                
     def listen_intsig(self, prefix=''):
-        if self._intsig is None:
-            self.bind_intsig()
         socket = self.context.socket(zmq.SUB)
         socket.setsockopt(zmq.SUBSCRIBE, prefix)
         socket.connect(self._intsig_endpoint)
@@ -32,9 +26,6 @@ class ZMQBase:
     
     def intsig(self, message):
         '''Send an in-process signal to all registered listeners'''
-        if self._intsig is None:
-            # No one listening, but bind anyway
-            self.bind_intsig()
         self._intsig.send(message)
     
         
@@ -51,10 +42,15 @@ class ZWorkProvider(ZMQBase):
         
         self.task_socket = self.context.socket(zmq.REP)
         self.results_socket = self.context.socket(zmq.PULL)
-        self.ann_out_socket = None
+        self.ann_out_socket = self.context.socket(zmq.PUB)
+        
+        #self.task_socket.setsockopt(zmq.LINGER,0)
+        #self.results_socket.setsockopt(zmq.LINGER,0)
+        #self.ann_out_socket.setsockopt(zmq.LINGER,0)
         
         self.task_socket.bind(task_endpoint)
         self.results_socket.bind(results_endpoint)
+        self.ann_out_socket.bind(ann_out_endpoint)
         
         self.do_dispatch = True
         self.dispatch_thread = None
@@ -62,13 +58,12 @@ class ZWorkProvider(ZMQBase):
         self.do_collect = True
         self.collect_thread = None
         
+        self.announce_interval = 100
+        
         # Timeouts are fatal
         self.test_mode = False
                 
     def announce_work(self):
-        if self.ann_out_socket is None:
-            self.ann_out_socket = self.context.socket(zmq.PUB)
-            self.ann_out_socket.connect(self.ann_out_endpoint)
         self.ann_out_socket.send('tasks available {}'.format(self.task_endpoint))
         
     def handle_work_request(self):
@@ -92,10 +87,7 @@ class ZWorkProvider(ZMQBase):
             self.task_socket.send('')
             
     def handle_results(self):
-        rsl = self.results_socket.recv_multipart()
-        if rsl[0] not in ('propagate',):
-            return
-        payload = cPickle.loads(rsl[1])
+        payload = self.results_socket.recv_pyobj()
         for result in payload:
             self.incoming_results.append(result)
         
@@ -131,11 +123,17 @@ class ZWorkProvider(ZMQBase):
             self.announce_work()
             
         while self.do_dispatch:
-            poll_results = set(poller.poll())
+            poll_results = set(poller.poll(self.announce_interval))
             if (self.task_socket, zmq.POLLIN) in poll_results:
                 self.handle_work_request()
             if (intsig, zmq.POLLIN) in poll_results and intsig.recv() == 'exit':
+                self.dispatch_thread = None
                 return
+            
+            if not poll_results:
+                # timeout
+                if self.outgoing_tasks:
+                    self.announce_work()
             
     def collect_loop(self):
         intsig = self.listen_intsig()
@@ -148,6 +146,7 @@ class ZWorkProvider(ZMQBase):
             if (self.results_socket, zmq.POLLIN) in poll_results:
                 self.handle_results()
             if (intsig, zmq.POLLIN) in poll_results and intsig.recv() == 'exit':
+                self.collect_thread = None
                 return
             
 class ZWorkConsumer(ZMQBase):
@@ -160,12 +159,49 @@ class ZWorkConsumer(ZMQBase):
         
         self.ann_in_endpoint = ann_in_endpoint
         
-        self.ann_in_socket = self.context.bind(zmq.SUB)
+        self.ann_in_socket = self.context.socket(zmq.SUB)
         self.ann_in_socket.setsockopt(zmq.SUBSCRIBE,'')
-        self.ann_in_socket.bind(self.ann_in_endpoint)
+        self.ann_in_socket.connect(self.ann_in_endpoint)
                 
         self.do_work = True
+        self.work_thread = None
+        
+    def shutdown(self):
+        self.do_work = False
+        self.intsig('exit')
+        
+    def work(self):
+        if self.work_thread is None:
+            self.work_thread = threading.Thread(target=self.work_loop)
+            self.work_thread.daemon = True
+            self.work_thread.start()
     
+    def retrieve_work(self, task_socket):
+        task_socket.send('request {}'.format(self.blocksize))
+        rpl = task_socket.recv_multipart()
+        if not rpl[0].startswith('tasks'):
+            log.error('invalid reply to task request; ignoring')
+            return (None, None)
+        
+        taskfields = rpl[0].split()
+        n_tasks = int(taskfields[1])
+        if not n_tasks:
+            return (None,None)
+        else:
+            results_endpoint = taskfields[2]
+        
+        if not (n_tasks <= self.blocksize):
+            log.error('invalid number of tasks received; ignoring')
+            return (None, None)
+        
+        payload = cPickle.loads(rpl[1])
+        if len(payload) != n_tasks:
+            log.error('payload contained wrong number of tasks; ignoring')
+            return (None, None)
+        
+        return (results_endpoint, payload)
+            
+        
     def work_loop(self):
         intsig = self.listen_intsig()
         poller = zmq.Poller()
@@ -176,6 +212,7 @@ class ZWorkConsumer(ZMQBase):
             poll_results = set(poller.poll())
             
             if (intsig, zmq.POLLIN) in poll_results and intsig.recv() == 'exit':
+                self.work_thread = None
                 return
             
             if (self.ann_in_socket, zmq.POLLIN) in poll_results:
@@ -185,29 +222,37 @@ class ZWorkConsumer(ZMQBase):
                     continue
                 _prefix, task_endpoint = msg.rsplit(None, 1)
                 
-                task_socket = self.context.connect(task_endpoint)
-                task_socket.send('request {}'.format(self.blocksize))
-                rpl = task_socket.recv_multipart()
-                if not rpl[0].startswith('tasks'):
-                    log.error('invalid reply to task request; ignoring')
-                    continue
+                task_socket = self.context.socket(zmq.REQ)
+                task_socket.connect(task_endpoint)
+                results_endpoint, payload = self.retrieve_work(task_socket)
                 
-                _prefix, ntaskstr, results_endpoint = rpl[0].split()
-                n_tasks = int(ntaskstr)
-                if not n_tasks: 
-                    continue
-                elif not (n_tasks <= self.blocksize):
-                    log.error('invalid number of tasks received; ignoring')
-                    continue
+                # Loop until we can get no more work from the given  provider, then sleep
+                # until the next announcement
+                while (results_endpoint, payload) != (None, None):
+                    # Connect the results socket (in case it fails)
+                    results_socket = self.context.socket(zmq.PUSH)
+                    results_socket.connect(results_endpoint)
+                    
+                    # Sort work by type
+                    tasks_by_type = {'propagate': []}
+                    
+                    for (task_type, task_data) in payload:
+                        try:
+                            tasks_by_type[task_type].append(task_data)
+                        except KeyError:
+                            log.error('invalid task type {} received; ignoring'.format(task_type))
+                            continue # with work sorting
+                    
+                    # Do work in chunks
+                    segments = tasks_by_type['propagate']
+                    self.propagator.propagate(segments)
+                    
+                    # Send it back
+                    results_socket.send_pyobj([('propagate', segment) for segment in segments])
+                    
+                    results_socket.close()
+                    
+                    # And ask for more
+                    results_endpoint, payload = self.retrieve_work(task_socket)
+                task_socket.close()
                 
-                payload = cPickle.loads(rpl[1])
-                if len(payload) != n_tasks:
-                    log.error('payload contained wrong number of tasks; ignoring')
-                    continue
-                
-                
-                
-                
-                
-                
-        
