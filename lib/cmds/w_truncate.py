5a44547afa8f95b8bb0ab9294c63ade522749ea7
Matt Zwier <mczwier@gmail.com>
2012-02-02 17:02:42 -0500

    Updated w_truncate to new wemd.rc functions, began same for w_steady_state, and removed remarkably obsolete w_sanity and w_segstats.

--- lib/cmds/w_truncate.py
+++ lib/cmds/w_truncate.py
@@ -1,35 +1,32 @@
 from __future__ import division, print_function
 
+import argparse
+
 import logging
 log = logging.getLogger('w_truncate')
 
 import wemd
 
-parser = wemd.rc.common_arg_parser()
-parser.add_argument('-n', '--iter', dest='iter', default=None, 
-                    help='Truncate after this iteration (by default remove the last iteration)')
-
+parser = argparse.ArgumentParser('w_truncate', description='''\
+Remove all iterations after a certain point in a  
+''')
+wemd.rc.add_args(parser)
+parser.add_argument('-n', '--iter', dest='n_iter', type=int,
+                    help='Truncate this iteration and those following.')
 args = parser.parse_args()
-
-wemd.rc.config_logging(args)
-runtime_config = wemd.rc.read_config(args.run_config_file)
-runtime_config.update_from_object(args)
-sim_manager = wemd.rc.load_sim_manager(runtime_config)
-sim_manager.load_data_manager()
-dm = sim_manager.data_manager
+wemd.rc.process_args(args, config_required=False)
+dm = wemd.rc.get_data_manager()
 dm.open_backing()
-max_iter = sim_manager.data_manager.current_iteration
+max_iter = dm.current_iteration
+n_iter = args.n_iter if args.n_iter > 0 else dm.current_iteration
 
-if args.iter is None:
-    iter = max_iter 
-else:
-    iter = int(args.iter)
-
-for i in xrange(iter, max_iter + 1):
+for i in xrange(n_iter, dm.current_iteration+1):
     dm.del_iter_group(i)
 
-dm.del_iter_summary(iter)
-sim_manager.data_manager.current_iteration = iter - 1
+dm.del_iter_summary(n_iter)
+dm.current_iteration = n_iter - 1
+
+print('simulation data truncated after iteration {}'.format(dm.current_iteration))
 
 dm.flush_backing()
 dm.close_backing()
