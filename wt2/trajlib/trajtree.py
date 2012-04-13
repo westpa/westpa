054521464bd503a896c9afe0e44b2b6e0551d097
Matt Zwier <mczwier@gmail.com>
2012-04-13 17:04:48 -0400

    Working trajectory tree construction and iteration.

--- wt2/trajlib/trajtree.py
+++ wt2/trajlib/trajtree.py
@@ -8,35 +8,51 @@ from collections import namedtuple, deque
 TrajID = namedtuple('TrajID', ['n_iter', 'seg_id'])
 
 class TrajNode:
-    def __init__(self, n_iter, seg_id, parent_id = None, prev = None):
+    def __init__(self, n_iter, seg_id, parent_id = None, prev = None, next = None, weight=None):
         self.n_iter = n_iter
         self.seg_id = seg_id
         self.parent_id = parent_id
+        self.weight = None
         
         # A reference to the previous segment's node, or None if this is a 
         # trajectory root
         self.prev = prev
         
+        # A list of references to child nodes
+        self.next = next or set()
+
+    def __hash__(self):
+        return id(self)
+        
     def __repr__(self):
-        return super(TrajNode,self).__repr__()[:-1] + 'n_iter={!r} seg_id={!r} parent_id={!r}>'\
-            .format(int(self.n_iter), long(self.seg_id), self.parent_id )
+        return super(TrajNode,self).__repr__()[:-1] + ' n_iter={!r} seg_id={!r} parent_id={!r}>'\
+            .format(int(self.n_iter), long(self.seg_id), long(self.parent_id))
+            
+    def trace(self):
+        '''Trace this trajectory back to its root from this node. Returns
+        a list of TrajNodes'''
+        return list(self.itrace())
+    
+    def itrace(self):
+        '''Trace this trajectory back to its root from this node. Returns
+        an iterator over TrajNodes.'''
+        node = self
+        yield self
+        while node.prev:
+            yield node.prev
+            node = node.prev
+            
+    def ftrace(self):
+        '''Trace this trajectory from its root to this node. Returns a list of TrajNodes'''
+        ftrace = self.trace()
+        ftrace.reverse()
+        return ftrace
+    
+    def iftrace(self):
+        '''Trace this trajectory from its root to this node. Returns an iterator over
+        TrajNodes, but must first store the trajectory in forward order.'''
+        return iter(self.ftrace())
         
-
-def reverse_traj_iter(terminal_node):
-    node = terminal_node
-    #print "yielding {!r}".format(node)
-    yield node
-    while node.prev:
-        #print "yielding {!r}".format(node.prev)
-        yield node.prev
-        node = node.prev
-        
-def forward_traj_iter(terminal_node):
-    traj = list(reverse_traj_iter(terminal_node))
-    traj.reverse()
-    for node in traj:
-        yield node        
-
 class TrajTree:
     def __init__(self):
         
@@ -47,19 +63,39 @@ class TrajTree:
         # The leaves (terminal segments) of the trajectories considered here
         # as a mapping from traj_ids to nodes
         self.leaves = {}
-        
+                        
     def __len__(self):
         '''Return the number of segments in this trajectory tree. O(N).'''
-        n_nodes = 0
         nodes = set(self.leaves.itervalues())
-        n_nodes += len(nodes)
         parents = {node.prev for node in nodes if node.prev is not None}
-        del nodes
+        nodes.update(parents)
         while parents:
-            n_nodes += len(parents)
             parents = {node.prev for node in parents if node.prev is not None}
-        return n_nodes
+            nodes.update(parents)
+        return len(nodes)
+
+    def __iter__(self):
+        '''Iterate over nodes, roughly depth-first'''
+        state_stack = deque()
+        
+        for node in self.roots.itervalues():
+            yield node
             
+            state_stack.append((node, set(node.next)))
+            
+            while state_stack:
+                node, remaining_children = state_stack.pop()
+                
+                while remaining_children:
+                    # Save current state before descending
+                    state_stack.append((node, remaining_children))
+                    
+                    node = remaining_children.pop()
+                    remaining_children = set(node.next)
+                    
+                    # visit new node, then descend on next iteration of this loop
+                    yield node
+                    
     def write_dot(self, dotfile):
         '''Write a Graphviz Dot input for this tree'''
         
@@ -88,60 +124,48 @@ def construct_tree(max_iter, get_matching_segs, data_manager = None):
         
     tree = TrajTree()
     # a mapping of seg_id in n_iter to parents (seg_id in n_iter-1)
-    parent_ids = {}
-    # a mapping of seg_id to corresponding nodes
-    next_iter_nodes = {}
     
+    
+    next_iter_nodes = []    
     for n_iter in xrange(max_iter, 0, -1):
         iter_group = data_manager.get_iter_group(n_iter)
         
-        this_iter_nodes = {}
-
-        # First, find and record leaves (terminal segments)        
         leaf_seg_ids = list(get_matching_segs(n_iter, iter_group))
         leaf_nodes = [TrajNode(n_iter, seg_id) for seg_id in leaf_seg_ids]
-        tree.leaves.update(((node.n_iter, node.seg_id), node) for node in leaf_nodes)
-        this_iter_nodes = {node.seg_id: node for node in leaf_nodes}
+        tree.leaves.update({(node.n_iter, node.seg_id): node for node in leaf_nodes})
         
-        # Next, create nodes for non-leaf segments in this iteration
-        # parent_ids contains a mapping from the seg_ids of the last iteration of this loop (n_iter+1)
-        # to parents in this iteration (n_iter)
-#        for (child_id, parent_id) in parent_ids.iteritems():
-#            next_iter_node = next_iter_nodes[child_id]
-#            next_iter_node.parent_id = parent_id
-#            
-#            if parent_id < 0:
-#                tree.roots[n_iter+1,child_id] = next_iter_node
-#            else:
-#                node = TrajNode(n_iter, parent_id)
-#                this_iter_nodes[parent_id] = node
-#                next_iter_node.prev = node
-
-        for (seg_id, node) in next_iter_nodes.iteritems():
-            parent_id = parent_ids[seg_id]
-            node.parent_id = parent_id
-            if parent_id < 0:
+        this_iter_nodes = list(leaf_nodes)
+        
+        new_nodes = {seg_id: TrajNode(n_iter, seg_id) 
+                     for seg_id in set(node.parent_id for node in next_iter_nodes if node.parent_id >= 0)}
+        this_iter_nodes.extend(new_nodes.itervalues())
+        
+        for node in next_iter_nodes:
+            if node.parent_id < 0:
                 tree.roots[node.n_iter,node.seg_id] = node
             else:
-                new_node = TrajNode(n_iter, parent_id)
-                this_iter_nodes[parent_id] = new_node
+                new_node = new_nodes[node.parent_id]
                 node.prev = new_node
+                new_node.next.add(node)
 
-        if this_iter_nodes:                        
-            parent_ids = data_manager.get_parent_ids(n_iter, this_iter_nodes.keys())
-            assert len(parent_ids) == len(this_iter_nodes)
-            #print('n_iter={!r}, parent_ids={!r}'.format(n_iter,parent_ids))
-        else:
-            parent_ids = {}
-        #print('n_iter={!r}, next_iter_nodes={!r}, this_iter_nodes={!r}'.format(n_iter, next_iter_nodes, this_iter_nodes))            
+        if this_iter_nodes:
+            seg_ids = [node.seg_id for node in this_iter_nodes]
+            parent_ids = data_manager.get_parent_ids(n_iter, seg_ids)
+            weights = data_manager.get_weights(n_iter,seg_ids)
+            for node, parent_id, weight in izip(this_iter_nodes, parent_ids, weights):
+                node.parent_id = parent_id
+                node.weight = weight
+            del parent_ids
+            
         next_iter_nodes = this_iter_nodes
+        del new_nodes, leaf_nodes
+        
         
     # roots at the beginning of the simulation are not recorded above, so do that now
-    for (child_id, parent_id) in parent_ids.iteritems():
-        assert parent_id < 0
-        node = next_iter_nodes[child_id]
+    for node in next_iter_nodes:
+        assert node.parent_id < 0
         assert node.n_iter == 1
-        tree.roots[1,child_id] = node
+        tree.roots[node.n_iter, node.seg_id] = node
         
     return tree
                 
@@ -159,22 +183,22 @@ def commonality_matrix(tree):
     
     leaves = list(tree.leaves.itervalues())
     for i in xrange(n_trajs):
-        traj_i = forward_traj_iter(leaves[i])
-        for j in xrange(i):
-            traj_j = forward_traj_iter(leaves[j])
+        traj_i = leaves[i].ftrace()
+        for j in xrange(i+1):
+            traj_j = leaves[j].ftrace()
             
-            for n_iter, (node_i, node_j) in enumerate(izip(traj_i, traj_j)):
-                print n_iter, i, j, (node_i.n_iter, node_i.seg_id), (node_j.n_iter, node_j.seg_id),
+            Cij = 0
+            
+            for node_i, node_j in izip(traj_i, traj_j):
                 if node_i is node_j:
-                    print 'match'
-                    C[i,j] += 1
+                    Cij += 1
                 else:
-                    print
                     break
+            
+            C[i,j] = C[j,i] = Cij    
 
     return leaves, C
-        
-        
+                        
           
         
         
