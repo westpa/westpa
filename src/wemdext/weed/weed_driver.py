Fixed several issues with weed plugin including backporting incorrect averaging fix from main repo

src/wemdext/weed/weed_driver.py
@@ -3,7 +3,7 @@ from __future__ import division; __metaclass__ = type
 import logging
 log = logging.getLogger(__name__)
 
-import math,numpy
+import numpy
 from itertools import izip
 
 from wemd.util.miscfn import vgetattr
@@ -17,12 +17,19 @@ class WEEDDriver:
 
         self.sim_manager = sim_manager
         self.data_manager = sim_manager.data_manager
-        
+        self.system = sim_manager.system
+
         self.do_reweight = wemd.rc.config.get_bool('weed.do_equilibrium_reweighting', False)
         self.windowsize = 0.5
         self.windowtype = 'fraction'
+        self.recalc_rates = wemd.rc.config.get_bool('weed.recalc_rates', False)
         
         windowsize = wemd.rc.config.get('weed.window_size', None)
+
+        self.max_windowsize = wemd.rc.config.get_int('weed.max_window_size', None)
+        if self.max_windowsize is not None:
+            log.info('Using max windowsize of {:d}'.format(self.max_windowsize))
+
         if windowsize is not None:
             if '.' in windowsize:
                 self.windowsize = float(windowsize)
@@ -44,8 +51,35 @@ class WEEDDriver:
             self.do_reweight = False 
         else:
             sim_manager.register_callback(sim_manager.prepare_new_segments, self.prepare_new_segments, self.priority)    
+
+    def calculate_rates(self,iter_group):
+        '''Calculate instantaneous rate based on current bin definitions for iter_group'''
         
-            
+        bins = self.system.curr_region_set.get_all_bins()
+        n_bins = len(bins)
+        
+        rates = numpy.zeros((n_bins,n_bins), numpy.float64)
+
+        with self.data_manager.lock:
+            pcoords = iter_group['pcoord']
+            assignments = numpy.empty((pcoords.shape[0], 2), numpy.int)
+            weights = iter_group['seg_index']['weight']
+
+            assignments[:,0] = self.system.curr_region_set.map_to_all_indices(pcoords[:,0,:]).astype(numpy.int)
+            assignments[:,1] = self.system.curr_region_set.map_to_all_indices(pcoords[:,-1,:]).astype(numpy.int)
+
+            populations = numpy.bincount(assignments[:,0], weights=weights, minlength=n_bins)
+            flattened_assign_ids = numpy.ravel_multi_index((assignments[:,0],assignments[:,1]), rates.shape)
+
+            fluxes = numpy.bincount(flattened_assign_ids, weights=weights, minlength=n_bins*n_bins)
+            fluxes = fluxes.reshape((n_bins,n_bins))
+
+        for i in xrange(0,n_bins):
+            if populations[i] > 0:
+                rates[i,:] = fluxes[i,:] / populations[i]
+
+        return rates 
+
     def get_rates(self, n_iter, bins):
         '''Get rates and associated uncertainties as of n_iter, according to the window size the user
         has selected (self.windowsize)'''
@@ -53,35 +87,55 @@ class WEEDDriver:
         n_bins = len(bins)
         
         if self.windowtype == 'fraction':
-            eff_windowsize = int(n_iter * self.windowsize)
-        else: # self.windowtype == 'fixed':
-            eff_windowsize = max(n_iter, self.windowsize or 0)
-        
-        rates = numpy.empty((eff_windowsize,n_bins,n_bins), numpy.float64)
+            if self.max_windowsize is not None:
+                eff_windowsize = min(self.max_windowsize,int(n_iter * self.windowsize))
+            else:
+                eff_windowsize = int(n_iter * self.windowsize)
                 
+        else: # self.windowtype == 'fixed':
+            eff_windowsize = min(n_iter, self.windowsize or 0)
+
+        rates = numpy.ma.masked_all((eff_windowsize,n_bins,n_bins), numpy.float64)
+
         n_used = 0
-        for n in xrange(n_iter, n_iter-eff_windowsize, -1):
+        for n in xrange(n_iter, max(n_iter-eff_windowsize,1), -1):
             log.debug('considering iteration {:d}'.format(n))
             with self.data_manager.lock:
                 iter_group = self.data_manager.get_iter_group(n)
-                rates_ds = iter_group['bin_rates']
+
+                if self.recalc_rates:
+                    rates_ds = self.calculate_rates(iter_group)
+                else:
+                    rates_ds = iter_group['bin_rates']
+
                 if rates_ds.shape != rates.shape[1:]:
                     # A bin topology change means we can't go any farther back
-                    self.sim_manager.status_stream.write(('Rate matrix for iteration {:d} is of the wrong shape; '
-                                                          +'stopping accumulation of average rate data.\n').format(n))
+                    wemd.rc.pstatus(('Rate matrix for iteration {:d} is of the wrong shape; '
+                                      +'stopping accumulation of average rate data.\n').format(n))
+                    wemd.rc.pflush()
                     break
-                
-                rates[n_used] = rates_ds[...]
+
+                # Mask rows where bin probability is zero
+                mrates = numpy.ma.array(rates_ds[...])
+
+                # Find bins with zero probability
+                binprobs = iter_group['bin_populations'][:,0]
+                zindx = numpy.where(binprobs == 0.0)[0]
+
+                mrates[zindx,zindx] = numpy.ma.masked
+                mrates = numpy.ma.mask_rows(mrates)
+
+                rates[n_used] = mrates
+
                 n_used += 1
 
-        avg_rates = rates[:n_used].mean(axis=0)
+        avg_rates = rates.mean(axis=0)
         if n_used == 1:
             unc_rates = avg_rates.copy()
         else:
-            unc_rates = rates[:n_used].std(axis=0) / math.sqrt(n_used)
-        return (avg_rates, unc_rates, n_used)
-        
-        
+            unc_rates = rates.std(axis=0) / numpy.sqrt(numpy.sum(~rates.mask,0))
+        return (avg_rates.data, unc_rates.data, n_used)
+
     def prepare_new_segments(self):
         n_iter = self.sim_manager.n_iter
         