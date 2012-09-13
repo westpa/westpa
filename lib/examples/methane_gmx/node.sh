924d29fc71431efe4833ed2f633d1fda32e52367
Matt Zwier <mczwier@gmail.com>
2012-09-13 18:45:01 -0400

    Updated methane_gmx example to reflect new work manager parameters, and added proper SGE $PE_HOSTFILE support to its batch script.

--- lib/examples/methane_gmx/node.sh
+++ lib/examples/methane_gmx/node.sh
@@ -2,7 +2,8 @@
 unset MODULE_VERSION_STACK MODULE_VERSION MODULEPATH LOADEDMODULES MODULESHOME module
 source /etc/profile.d/modules.sh
 
-cd $SIM_ROOT
+cd $WEMD_SIM_ROOT
 source env.sh
 
+set -x
 $WEMD_ROOT/bin/w_run "$@"
--- lib/examples/methane_gmx/runwe_sge.sh
+++ lib/examples/methane_gmx/runwe_sge.sh
@@ -2,30 +2,38 @@
 #$ -N CH4bind
 #$ -q compute48.q
 #$ -l h_rt=36:00:00
-#$ -pe 48way 48
+#$ -pe 8way 16
 #$ -V
-
-# Set this to "1" if using multiple nodes; otherwise 0
-MULTINODE=0
+#$ -cwd
 
 source /opt/modules/sgefix.sh
-source env.sh
+source env.sh || exit 1
 
-master=$(hostname)
+cd $WEMD_SIM_ROOT
+SERVER_INFO=$WEMD_SIM_ROOT/wemd_zmq_info-$JOB_ID.json
 
-# Switch depending on whether we are using multiple nodes
-if [[ $MULTINODE == "1"]] ; then
-    # spawn workers
-    for host in $(cat $TMPDIR/machines | uniq); do
-        echo "starting WEMD node worker on $host"
-        qrsh -inherit -V $host $PWD/node.sh --node -H $master </dev/null &> wemd-$JOB_ID-$host.log &
-    done
+# start server
+$WEMD_ROOT/bin/w_run --wm-work-manager=zmq --wm-n-workers=0 --wm-zmq-mode=server --wm-zmq-server-info=$SERVER_INFO &> wemd-$JOB_ID.log &
 
-    # spawn master
-    echo "starting master on $master"
-    $WEMD_ROOT/bin/w_run --master -H $master -n 0 &> wemd-$JOB_ID.log </dev/null &
-    wait
-else
-    $WEMD_ROOT/bin/w_run --work-manager=threads &> wemd-$JOB_ID.log
+# wait on host info file up to one minute
+for ((n=0; n<60; n++)); do
+    if [ -e $SERVER_INFO ] ; then
+        echo "== server info file $SERVER_INFO =="
+        cat $SERVER_INFO
+        break
+    fi
+    sleep 1
+done
+
+# exit if host info file doesn't appear in one minute
+if ! [ -e $SERVER_INFO ] ; then
+    echo 'server failed to start'
+    exit 1
 fi
 
+# start clients, with the proper number of cores on each
+while read -r machine ncores _j _k ; do 
+   qrsh -inherit -V $machine $PWD/node-ltc1.sh --wm-work-manager=zmq --wm-zmq-mode=client --wm-n-workers=$ncores --wm-zmq-server-info=$SERVER_INFO &> wemd-$JOB_ID-$machine.log &
+done < $PE_HOSTFILE
+
+wait
