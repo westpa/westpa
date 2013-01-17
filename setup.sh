39fe4116469a52288bd77e41983aeff064ae45a7
Matt Zwier <mczwier@gmail.com>
2013-01-17 13:15:21 -0500

    Tweaked setup.sh to delete .so files from all directories, including h5py's.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+find . -name \*.so -print0 | xargs -0 rm &> /dev/null
 
 if [[ -d lib/h5py ]] ; then
     echo "using custom h5py located in $PWD/lib/h5py"
@@ -20,7 +21,6 @@ fi
 
 for d in src lib/west_tools; do
     cd $d
-    find . -name \*.so -print0 | xargs -0 rm
     python setup.py build_ext --inplace
     cd -
 done
