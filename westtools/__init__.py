0dc3e223ce1c2728ffb71732e1749a9ef8dd22a2
Matt Zwier <mczwier@gmail.com>
2013-04-23 16:57:37 -0400

    Removed dangling UsesMCBS import

--- westtools/__init__.py
+++ westtools/__init__.py
@@ -1,3 +1,3 @@
 '''westtools -- classes for implementing command-line tools for WESTPA'''
 
-from tool_classes import UsesMCBS, WESTDataReader, WESTTool
+from tool_classes import WESTDataReader, WESTTool
