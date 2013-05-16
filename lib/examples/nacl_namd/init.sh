f512799faa4dd7e213aff2a4b1acc5460e5c8ca9
ktd3 <devnull@localhost>
2013-05-16 11:37:33 -0400

    correction to nacl_namd tutorial

--- lib/examples/nacl_namd/init.sh
+++ lib/examples/nacl_namd/init.sh
@@ -12,6 +12,6 @@ rm -f   system.h5 west.h5 seg_logs.tar
 mkdir   seg_logs traj_segs istates
 
 BSTATE_ARGS="--bstate-file bstates/bstates.txt"
-TSTATE_ARGS="--tstate bound,2.8"
+TSTATE_ARGS="--tstate bound,2.8 --tstate unbound,16.9"
 
 $WEST_ROOT/bin/w_init $BSTATE_ARGS $TSTATE_ARGS --segs-per-state 48 --work-manager=threads "$@"
