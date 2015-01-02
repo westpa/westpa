16775003bca7d323fa16cb9a75bfca6aa0e8b6ef
Karl Debiec <karl.t.debiec@gmail.com>
2015-01-02 16:27:05 -0500

    Major updates and revisions to nacl_amb tutorial
    Former-commit-id: a52b4981bd6f288272a4ef708d339eb86df22b7e

--- lib/examples/nacl_amb/env.sh
+++ lib/examples/nacl_amb/env.sh
@@ -1,9 +1,13 @@
-# This file defines where WEST and GROMACS can be found
+# This file defines where WEST and Amber can be found
 # Modify to taste
 
 # Inform WEST where to find Amber
 export AMBERHOME=/path/to/amberhome
-export PATH=$PATH:/path/to/amberhome/bin
+export PATH=$AMBERHOME/bin:$PATH
+
+# Set environment variables for Amber for convenience
+export PMEMD=$(which pmemd)
+export CPPTRAJ=$(which cpptraj)
 
 # Inform WEST where to find Python and our other scripts where to find WEST
 export WEST_PYTHON=$(which python2.7)
@@ -13,5 +17,7 @@ export WEST_ROOT=$(readlink -f $PWD/../../..)
 if [[ -z "$WEST_SIM_ROOT" ]]; then
     export WEST_SIM_ROOT="$PWD"
 fi
+
+# Set simulation name
 export SIM_NAME=$(basename $WEST_SIM_ROOT)
 echo "simulation $SIM_NAME root is $WEST_SIM_ROOT"
