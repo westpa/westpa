611cecd1f71a8fd49d6d57b3c6c26855918e7041
ajoshpratt@gmail.com <ajoshpratt@gmail.com>
2015-06-02 21:45:11 -0400

    Beginning of function to average generic datasets.

--- avg/__init__.py
+++ avg/__init__.py
@@ -5,3 +5,7 @@ Set of tools for use with averaging together multiple WESTPA simulations.
 import logging
 log = logging.getLogger(__name__)
 
+
+def _average_2D_data_slice(slice, dataset):
+    # A function which takes in a slice, and a dataset, and adds on 
+    # to it.
