15493b35cf8a04e9e9808f866991080c4bd078bf
Patrick Pisciuneri <phpisciuneri@gmail.com>
2015-09-28 14:45:16 -0400

    added server threads for dispatching and receiving.

--- work_managers/mpi.py
+++ work_managers/mpi.py
@@ -1,6 +1,9 @@
+# TODO: License 
+#
+#
 from __future__ import division, print_function; __metaclass__ = type
 
-import logging, sys, re
+import logging, sys, re, threading
 
 from collections import deque
 from mpi4py import MPI
@@ -39,8 +42,13 @@ class MPIWorkManager( WorkManager ):
         
 
     def __new__( cls ):
+        print( 'MPIWorkManager.__new__()' )
+        print( 'Is_initialized(): %s' % MPI.Is_initialized() )
+        print( 'Is_thread_main(): %s' % MPI.Is_thread_main() )
         rank = MPI.COMM_WORLD.Get_rank()
         size = MPI.COMM_WORLD.Get_size()
+        
+        
         if size == 1:
             return super( MPIWorkManager, cls ).__new__( Serial )
         elif rank == 0:
@@ -83,6 +91,9 @@ class Serial( MPIWorkManager ):
         
         
     def submit( self, fn, args=None, kwargs=None ):
+        # TODO: add docstring
+        """
+        """
         ft = WMFuture()
         try:
             result = fn( *(args if args is not None else ()), 
@@ -105,50 +116,98 @@ class Master( MPIWorkManager ):
     def __init__( self ):
         super( Master, self ).__init__()
         log.debug( 'Master construction' )
-        print( 'Master construction')
+        print( 'Master.__init__()' )
 
-        # doc        
+        # TODO: doc        
         self.nslaves   = self.size - 1        
         self.slaveIDs  = range( 1, self.size )
-        self.init_dests = self.slaveIDs[:]
+        
+        self.dests     = deque( self.slaveIDs ) 
+        self.tasks     = deque()
+        self.nPending  = 0 # number of unmatched send/recv pairs
+        
+        self.shutItDown = False
+        
         assert self.nslaves == len( self.slaveIDs )
         
         self.pending_futures = dict()
-        
+        self.workers = []
 
-    def _dispatch( self, task ):
-        """The first self.nslaves tasks are dispatched to the individual
-        slaves.  Subsequently, slaves get more work when they are finished 
-        with a given task.
-        """
-        log.debug( 'Master._dispatch() %s' )
         
-        # the first self.nclient jobs
-        if self.init_dests:
-            # send work
-            self.comm.isend( task, dest=self.init_dests.pop(), tag=self.task_tag )
-        else:
-            log.info( "Master._dispatch() waiting to receive something..." )
-            # who is done?
-            stat = MPI.Status()
-            ( tid, msg, val ) = self.comm.recv( source=MPI.ANY_SOURCE, tag=self.result_tag, status=stat )
+    def startup( self ):
+        # TODO: add docstring
+        """
+        """
+        log.debug( 'Master.startup()' )
+        print( 'Master.startup()' )
+        
+        if not self.running:
+            self.workers.append( threading.Thread( name='dispatcher', target=self._dispatcher) )
+            self.workers.append( threading.Thread( name='receiver', target=self._receiver ) )
             
-            log.info( "Master._dispatch() received results." )
-            
-            # send the new task to the now idle client
-            dest = stat.Get_source()
-            self.comm.isend( task, dest=dest, tag=self.task_tag )
-            
-            # TODO: update the future of the received result
-            ft = self.pending_futures.pop( tid )
-            if msg == 'exception':
-                ft._set_exception( *val )
-            else:
-                ft.set_result( val )
+            for t in self.workers:
+                log.info( 'Starting thread: %s' % t.getName() )
+                t.start()
                 
-        log.debug( 'Leaving Master._dispatch()' )
+            self.running = True
+        
+        
+    def _dispatcher( self ):
+        # TODO: add docstring
+        """
+        """
+        log.debug( 'Master._dispatcher()' )
+        assert( MPI.Is_thread_main() == False )
+        assert( threading.currentThread().getName() == "dispatcher" )
+        
+        while not self.shutItDown:
             
-        # TODO: check the status of the non-blocking sends
+            req = []
+            # do we have work and somewhere to send it?
+            while self.tasks and self.dests:
+                
+                ## TODO: do we need to lock here
+                task = self.tasks.popleft()
+                sendTo = self.dests.popleft()
+                self.nPending += 1
+                print( "_dispatcher.nPending: %s" % self.nPending )
+                ## unlock
+                
+                req.append( self.comm.isend( task, dest=sendTo, tag=self.task_tag ) )
+                
+            # make sure all sends completed
+            MPI.Request.Waitall( requests=req )
+        
+    
+    def _receiver( self ):
+        # TODO: add docstring
+        """
+        """
+        log.debug( 'Master._receiver()' )
+        assert( MPI.Is_thread_main() == False )
+        assert( threading.currentThread().getName() == "receiver" )
+        
+        while not self.shutItDown:
+            
+            # are we waiting on any results?
+            while self.nPending:
+            
+                stat = MPI.Status()
+                ( tid, msg, val ) = self.comm.recv( source=MPI.ANY_SOURCE, 
+                                        tag=self.result_tag, status=stat )
+                print( "Master._receiver() received result" )
+            
+                ft = self.pending_futures.pop( tid )
+                if msg == 'exception':
+                    ft._set_exception( val )
+                else:
+                    ft._set_result( val )
+            
+                ## TODO: do we need a lock here
+                self.dests.append( stat.Get_source() )
+                self.nPending -= 1
+                print( "_receiver.nPending: %s" % self.nPending )
+                ## unlock
         
     
 
@@ -160,10 +219,8 @@ class Master( MPIWorkManager ):
         
         ft = WMFuture()
         task_id = ft.task_id
-        task = Task( task_id, fn, args, kwargs )
+        self.tasks.append( Task( task_id, fn, args, kwargs ) )
         self.pending_futures[task_id] = ft
-        
-        self._dispatch( task )
         
         return ft
 
@@ -174,12 +231,26 @@ class Master( MPIWorkManager ):
         log.debug( 'Master.shutdown()' )
         print( 'Master.shutdown()' )
         
-        req = [ MPI.REQUEST_NULL ]*self.nslaves       
+        # wait on any unfinished work
+        while self.pending_futures:
+            pass
         
+        # send shutdown msg to all slaves
+        req = [ MPI.REQUEST_NULL ]*self.nslaves        
         for rank in self.slaveIDs:
             req[rank-1] = self.comm.isend( MPI.BOTTOM, dest=rank, tag=self.shutdown_tag )
             
         MPI.Request.Waitall( requests=req )
+        
+        # stop threads
+        self.shutItDown = True
+        
+        for t in self.workers:
+            print( 'Stopping thread: %s' % t.getName() )
+            log.info( 'Stopping thread: %s' % t.getName() )
+            t.join()
+        
+        self.running = False
     
 
 # +-------+
@@ -203,24 +274,24 @@ class Slave( MPIWorkManager ):
         """Do each task as it comes in.  The completion of a task is
         notice to the master that more work is welcome.
         """
-
-        # TODO: remove - temporary for testing        
-        import time        
+        log.debug( 'Slave.clockIn()' )
         
-        log.debug( 'Slave.clockIn() %s' % self.rank )
-        log.info( 'Slave %s clocking in' % self.rank )
+        log.info( 'Slave %s clocking in.' % self.rank )
         comm = self.comm
         
         while True:
             
+            print( "Slave waiting for work..." )
             stat = MPI.Status()
             task = comm.recv( source=self.masterID, 
                              tag=MPI.ANY_TAG,
                              status=stat )
+            print ( "Slave got work" )
 
             log.info( "Slave.clockIn() received task." )
                              
             tag = stat.Get_tag()
+            print( "Slave msg tag: %s" % tag )
             
             if tag == self.task_tag:
                 
@@ -234,20 +305,13 @@ class Slave( MPIWorkManager ):
                 else:
                     ro = ( task.task_id, 'result', rv )
                 
-                # TODO: remove - temporary for testing
-                # simulate some work done
-                #time.sleep( 5 )
-                #print( "I am client %s.  I just did some work." % self.rank )
-                
                 # send result back to master
                 comm.isend( ro, dest=self.masterID, tag=self.result_tag )
                 
             if tag == self.shutdown_tag:
+                print( 'Slave %s clocking out' % self.rank )
                 log.info( 'Slave %s clocking out' % self.rank )
                 return
-            
-
-            log.debug( "Slave.clockIn() leaving..." )
 
 
     @property
@@ -259,14 +323,14 @@ class Slave( MPIWorkManager ):
 
 
 
-
 """
 m = MPIWorkManager()
     
 with m:
+    
+    print( '%s' % MPI.Is_thread_main() )
     # submit a series of tasks as in the code
-    for task_id in range( 10 ):
+    for task_id in range( 5 ):
         t = Task( task_id, max, None, None )
         m.submit( t.fn, t.args, t.kwargs )
 """
-
