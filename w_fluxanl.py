6536ed8bfb8691001ed08ccfd4bdee8d3e3c9775
ajpratt <ajp105@pitt.edu>
2016-06-28 12:42:43 -0400

    w_fluxanl fixed. Works with the mclib, now.

--- w_fluxanl.py
+++ w_fluxanl.py
@@ -256,9 +256,9 @@ the true value of ``tau``.
             # Calculate overall averages and CIs
             #avg, lb_ci, ub_ci, correl_len = mclib.mcbs_ci_correl(fluxes, numpy.mean, self.alpha, self.n_sets,
             #                                                     autocorrel_alpha=self.autocorrel_alpha, subsample=numpy.mean)
-            avg, lb_ci, ub_ci, correl_len = mclib.mcbs_ci_correl_rw({'dataset': fluxes}, estimator=(lambda stride, dataset: numpy.mean(dataset)), self.alpha, self.n_sets,
+            avg, lb_ci, ub_ci, sterr, correl_len = mclib.mcbs_ci_correl_rw({'dataset': fluxes}, estimator=(lambda stride, dataset: numpy.mean(dataset)), alpha=self.alpha, n_sets=self.n_sets,
                                                                  autocorrel_alpha=self.autocorrel_alpha, subsample=numpy.mean, pre_calculated=fluxes, correl=False)
-            avg_fluxdata[itarget] = (self.iter_range.iter_start, self.iter_range.iter_stop, avg, lb_ci, ub_ci, correl_len)
+            avg_fluxdata[itarget] = (self.iter_range.iter_start, self.iter_range.iter_stop, avg, lb_ci, ub_ci, sterr, correl_len)
             westpa.rc.pstatus('target {!r}:'.format(target_label))
             westpa.rc.pstatus('  correlation length = {} tau'.format(correl_len))
             westpa.rc.pstatus('  mean flux and CI   = {:e} ({:e},{:e}) tau^(-1)'.format(avg,lb_ci,ub_ci))
@@ -301,13 +301,14 @@ the true value of ``tau``.
                 #avg, ci_lb, ci_ub, correl_len = mclib.mcbs_ci_correl(fluxes, numpy.mean, self.alpha, self.n_sets,
                 #                                                     autocorrel_alpha = self.autocorrel_alpha,
                 #                                                     subsample=numpy.mean)
-                avg, ci_lb, ci_ub, correl_len = mclib.mcbs_ci_correl({'dataset': fluxes}, estimator=(lambda stride, dataset: numpy.mean(dataset), self.alpha, self.n_sets,
+                avg, ci_lb, ci_ub, sterr, correl_len = mclib.mcbs_ci_correl_rw({'dataset': fluxes}, estimator=(lambda stride, dataset: numpy.mean(dataset)), alpha=self.alpha, n_sets=self.n_sets,
                                                                      autocorrel_alpha = self.autocorrel_alpha,
-                                                                     subsample=numpy.mean, pre_calculated: fluxes, correl=False)
+                                                                     subsample=numpy.mean, pre_calculated=fluxes, correl=False)
                 cis[iblock]['iter_start'] = iter_start
                 cis[iblock]['iter_stop']  = block_iter_stop
                 cis[iblock]['expected'], cis[iblock]['ci_lbound'], cis[iblock]['ci_ubound'] = avg, ci_lb, ci_ub
                 cis[iblock]['corr_len'] = correl_len
+                cis[iblock]['sterr'] = sterr
                 
                 del fluxes
 
