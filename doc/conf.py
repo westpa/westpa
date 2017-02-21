a5f2297cd7420e33772f1d8fb6eb5b157de64eb5
ASinanSaglam <asinansaglam@gmail.com>
2017-02-21 12:58:00 -0500

    Some overall changes to Sphinx page

--- doc/conf.py
+++ doc/conf.py
@@ -110,7 +110,12 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+        'logo':'WESTPA2.jpg',
+        'page_width': '1000px',
+        'sidebar_width': '240px',
+        'sidebar_headers': '#cecece'
+        }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
