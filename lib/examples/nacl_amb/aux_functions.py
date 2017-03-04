d2e1f3e14fb15bfcbae84d054da7ed43d8160752
Alex DeGrave <ajd98@pitt.edu>
2017-03-04 17:44:49 -0500

    Example files now use YAML to specify the binning scheme and other simulation properties. Moved auxilliary functions from system.py to aux_functions.py, and removed system.py
    Former-commit-id: f0684b2064aae9569e9371cb2a1057d120eaee7d

--- lib/examples/nacl_amb/aux_functions.py
+++ lib/examples/nacl_amb/aux_functions.py
@@ -1,34 +1,5 @@
-from __future__ import division, print_function; __metaclass__ = type
-import os, sys, math, itertools
+#!/usr/bin/env python
 import numpy
-import west
-from west import WESTSystem
-from westpa.binning import RectilinearBinMapper
-
-import logging
-log = logging.getLogger(__name__)
-log.debug('loading module %r' % __name__)
-
-class System(WESTSystem):
-    """
-    System for sodium chloride association
-    """
-
-    def initialize(self):
-        """
-        Initializes system
-        """
-        self.pcoord_ndim  = 1
-        self.pcoord_len   = 6
-        self.pcoord_dtype = numpy.float32
-        binbounds         = [ 0.00, 2.80, 2.88, 3.00, 3.10, 3.29, 3.79, 3.94,
-                              4.12, 4.39, 5.43, 5.90, 6.90, 7.90, 8.90, 9.90,
-                             10.90,11.90,12.90,13.90,14.90,15.90,float('inf')]
-        self.bin_mapper   = RectilinearBinMapper([binbounds])
-
-        self.bin_target_counts      = numpy.empty((self.bin_mapper.nbins,),
-                                        numpy.int)
-        self.bin_target_counts[...] = 24
 
 def coord_loader(fieldname, coord_filename, segment, single_point=False):
     """
