c0ca432065d680b72703563626e42d69d78b1330
Adam Pratt <ajoshpratt@gmail.com>
2017-03-19 16:02:15 -0400

    Moved a LOT of other functionality into westtools/wipi.py

--- lib/west_tools/westtools/__init__.py
+++ lib/west_tools/westtools/__init__.py
@@ -6,4 +6,4 @@ from selected_segs import SegSelector
 from binning import BinMappingComponent, mapper_from_dict
 from progress import ProgressIndicatorComponent
 from plot import Plotter
-from dtypes import WIPIDataset
+from wipi import WIPIDataset, KineticsIteration, __get_data_for_iteration__, WIPIScheme
