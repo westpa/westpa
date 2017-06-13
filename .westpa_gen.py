90908a7539c3fe73da4d61db82260c71f9acc5b5
kimwong <kimwong@pitt.edu>
2017-06-13 13:19:47 -0400

    Update .westpa_gen.py to use PREFIX

--- .westpa_gen.py
+++ .westpa_gen.py
@@ -12,6 +12,11 @@ export PATH=$WEST_BIN:$PATH
 \n'''
 
 with open('westpa.sh', 'w') as f:
-    f.write(template.format(WESTROOT=os.environ.get('PWD'),
-                            WESTBIN=os.path.join(os.environ.get('PWD'), 'bin'),
+    f.write(template.format(WESTROOT=os.environ.get('PREFIX'),
+                            WESTBIN=os.path.join(os.environ.get('PREFIX'), 'bin'),
                             WESTPYTHON=os.environ.get('WEST_PYTHON','')))
+
+#kimwong with open('westpa.sh', 'w') as f:
+#kimwong    f.write(template.format(WESTROOT=os.environ.get('PWD'),
+#kimwong                            WESTBIN=os.path.join(os.environ.get('PWD'), 'bin'),
+#kimwong                            WESTPYTHON=os.environ.get('WEST_PYTHON','')))
