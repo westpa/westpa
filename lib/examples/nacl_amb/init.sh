01a62dd94a89e87c088624cedeacbf5d6f730610
Alex DeGrave <ajd98@pitt.edu>
2017-08-21 10:31:31 -0400

    Add lines for copying initial coordinates from /prep

--- lib/examples/nacl_amb/init.sh
+++ lib/examples/nacl_amb/init.sh
@@ -11,8 +11,10 @@ rm -Rf  traj_segs$SFX seg_logs$SFX istates$SFX & disown %1
 rm -f   system.h5 west.h5 seg_logs.tar
 mkdir   seg_logs traj_segs istates
 
+cp prep/4_eq2/4_eq2.rst bstates/nacl_init.rst
+
 BSTATE_ARGS="--bstate-file bstates/bstates.txt"
-TSTATE_ARGS="--tstate bound,1.8 --tstate unbound,16.9"
+TSTATE_ARGS="--tstate bound,1.8"
 
 $WEST_ROOT/bin/w_init $BSTATE_ARGS $TSTATE_ARGS --segs-per-state 24 \
   --work-manager=threads "$@"
