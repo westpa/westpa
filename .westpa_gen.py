47a236d5786c452398a2429fd9ec9ca29cff63ca
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2017-08-21 16:42:56 -0400

    add support for csh and derived shells

--- .westpa_gen.py
+++ .westpa_gen.py
@@ -1,6 +1,6 @@
 import os
 
-template = '''\
+template_sh = '''\
 
 # source this file to set the environment variables in your shell.
 
@@ -11,7 +11,26 @@ export WEST_PYTHON={WESTPYTHON}
 export PATH=$WEST_BIN:$PATH
 \n'''
 
+template_csh = '''\
+
+# source this file to set the environment variables in your shell.
+
+setenv WEST_ROOT "{WESTROOT}"
+setenv WEST_BIN "{WESTBIN}"
+setenv WEST_PYTHON "{WESTPYTHON}"
+
+\n'''
+
 with open('westpa.sh', 'w') as f:
-    f.write(template.format(WESTROOT=os.environ.get('PWD'),
+    f.write(template_sh.format(WESTROOT=os.environ.get('PWD'),
                             WESTBIN=os.path.join(os.environ.get('PWD'), 'bin'),
                             WESTPYTHON=os.environ.get('WEST_PYTHON','')))
+
+with open('westpa.csh', 'w') as f:
+    f.write(template_csh.format(WESTROOT=os.environ.get('PWD'),
+                            WESTBIN=os.path.join(os.environ.get('PWD'), 'bin'),
+                            WESTPYTHON=os.environ.get('WEST_PYTHON','')))
+    string = 'setenv PATH "' + os.path.join(os.environ.get('PWD'), 'bin') +\
+            ':$PATH"\n'
+
+    f.write(string)
