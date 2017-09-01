25ba0e16bc35a186432b4f4ca98365a9cd8559ae
Alex DeGrave <ajd98@pitt.edu>
2017-09-01 15:48:52 -0400

    Added files and scripts for preparing system
    Former-commit-id: f914997e6d25514dd98a67906ae64012768a694c

---
+++ lib/examples/nacl_gmx/prep/3_min/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+gmx grompp \
+  -f minimize.mdp \
+  -c ../2_solvate/nacl_solvated.gro \
+  -p ../1_pdb2gmx/topol.top \
+  -o 3_min.tpr
+
+gmx mdrun \
+  -deffnm 3_min\
+  -mp ../1_pdb2gmx/topol.top\
+  -v 
+
