6d47f36b855bc3035c1c09b1c2b9159110c626b1
Alex DeGrave <ajd98@pitt.edu>
2017-09-01 16:57:37 -0400

    Consistency with amber nacl tutorial; added docstring
    Former-commit-id: 46f5280902b19cd26ea0cc313f50aec0737c2089

--- lib/examples/nacl_gmx/aux_functions.py
+++ lib/examples/nacl_gmx/aux_functions.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-#
-# aux_functions.py
-#
-# This Python module define an auxilliary function used during the WESTPA 
-# simulation. This function informs WESTPA how to load atomic coordinate data 
-# from GROMACS trajectory files.
 import numpy
 
 def coord_loader(fieldname, coord_filename, segment, single_point=False):
@@ -19,10 +13,11 @@ def coord_loader(fieldname, coord_filename, segment, single_point=False):
                            (should always be false)
     """
     # Load coordinates
-    n_frames = 6
+    n_frames = 51
     n_atoms  = 2
     coord    = numpy.loadtxt(coord_filename, dtype = numpy.float32)
     coord    = numpy.reshape(coord, (n_frames, n_atoms, 3))
 
     # Save to hdf5
     segment.data[fieldname] = coord
+
