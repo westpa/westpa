c2bc9bbf08c4ab7c166991ea77707f9827e2d82a
Alex DeGrave <ajd98@pitt.edu>
2017-09-01 14:15:01 -0400

    Added files for calculating position of bound state

---
+++ lib/examples/nacl_gmx/bound_state_definition/1_pdb2gmx/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Check for gmx
+#GMX=$(which gmx)
+#if [ ! -n "${GMX}"]; then
+#  echo "Gromacs not found! Make sure your Gromacs installation is in your path."
+#  exit 1
+#fi
+#GROMACS_DIR=$(basename ${GMX} /bin/gmx)
+#cp ${GROMACS_DIR}/share/gromacs/top/amber03.ff/tip3p.itp tip3p_jc2008.ff/
+
+ln -s ../../gromacs_config/tip3p_ionsjc2008.ff .
+
+gmx pdb2gmx \
+  -f nacl_no_solvent.pdb \
+  -ff tip3p_ionsjc2008 \
+  -water tip3p \
+  -o nacl_no_solvent_processed.gro
