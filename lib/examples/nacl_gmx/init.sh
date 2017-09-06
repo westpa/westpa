a7750a567ee7e3de9743394c50e647e5f269aed2
Alex DeGrave <ajd98@pitt.edu>
2017-09-06 11:17:14 -0400

    Symlink files from ./prep directory when initializing simulation

--- lib/examples/nacl_gmx/init.sh
+++ lib/examples/nacl_gmx/init.sh
@@ -22,6 +22,20 @@ pkill -9 -f w_run
 rm -rf traj_segs seg_logs istates west.h5 
 mkdir   seg_logs traj_segs istates
 
+# Symlink the results of the equilibration to the bstates directory
+rm -rf bstates/unbound
+mkdir bstates/unbound
+cd bstates/unbound || exit 1
+ln -s ../../prep/5_eq2/5_eq2.gro seg.gro
+ln -s ../../prep/5_eq2/5_eq2.trr seg.trr
+ln -s ../../prep/5_eq2/5_eq2.edr seg.edr
+cd ../../
+
+# Also symlink the topology file
+cd gromacs_config || exit 1
+ln -s ../prep/1_pdb2gmx/topol.top nacl.top
+cd ../
+
 # Define the arguments for the basis states (used for generating initial 
 # states; in this case we only have one), and target states (used for
 # knowing when to recycle trajectories). In this example, we recycle
