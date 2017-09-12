1397e1080c207235f7b7ca83662cbc8b424d48db
Alex DeGrave <ajd98@pitt.edu>
2017-09-12 13:20:49 -0400

    Added checks to make sure CHARMM force field is found

--- lib/examples/nacl_namd/prep/1_psf/run.sh
+++ lib/examples/nacl_namd/prep/1_psf/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Make sure the CHARMM force field is in the right spot:
+if [ ! -d ../../namd_config/toppar ]; then
+  echo "CHARMM force field not found! Download the CHARMM36 force field, untar "
+  echo "it, and place the 'toppar' directory in namd_config."
+  exit 1
+fi
+
 cat ../../namd_config/toppar/toppar_water_ions.str \
   | grep -v -e "^set" \
   | grep -v -e "^if" \
