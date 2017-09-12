35c61a5d0d9f2f58ccdb5fac1a7dfa8d7f71b1a0
Alex DeGrave <ajd98@pitt.edu>
2017-09-12 13:22:18 -0400

    Updated error message

--- lib/examples/nacl_namd/prep/1_psf/run.sh
+++ lib/examples/nacl_namd/prep/1_psf/run.sh
@@ -3,7 +3,7 @@
 # Make sure the CHARMM force field is in the right spot:
 if [ ! -d ../../namd_config/toppar ]; then
   echo "CHARMM force field not found! Download the CHARMM36 force field, untar "
-  echo "it, and place the 'toppar' directory in namd_config."
+  echo "it, and place the 'toppar' directory in ../../namd_config."
   exit 1
 fi
 
