9d814e991ac91af1ea335ab07e393e7111181642
Page Harrison <page.harrison.o@gmail.com>
2019-06-12 16:06:32 -0400

    Work on go() function
    Former-commit-id: 5da8e3150fcb6c45ecadf1f1ff2eb0cbb254c4c1

--- lib/west_tools/w_fptd.py
+++ lib/west_tools/w_fptd.py
@@ -17,6 +17,8 @@ from westtools import WESTDataReader
     w_assign.py: WESTDataReader()
     ^see line 449
 
+    #look at: w_assign
+
 """
 
 class WFptd(WESTTool):
@@ -24,95 +26,107 @@ class WFptd(WESTTool):
     def __init__(self):
         super(WFptd,self).__init__()
         self.data_reader = WESTDataReader()
+        self.input_file = None
+        self.output_hdf5file = None
+
         self.n_bins = 0
         self.init_bins = []
         self.target_bins = []
         self.cbins = []
+
+
         self.K = None
+        self.iter = 1000000
         self.merged_rates = []
         self.description = 'Calculate the first passage time distribution for transitions \
             between two states.'
-        make_parser()
-        add_args(parser)
+
+        # main(self) (inherited) calls make_parser_and_process() then calls go()
 
     # @ self, parser
     def add_args(self, parser):
+
         self.data_reader.add_args(parser) # get HDF5 from user
         parser.add_argument('nbins', help='Number of bins', metavar="num_bins", type=int)
         cgroup = parser.add_argument_group('Calculation options')
         cgroup.add_argument('-i', required=True, help='Initial state bins', metavar='init_bins', type=int, nargs='+')
         cgroup.add_argument('-f', required=True, help='Final state bins', metavar='final_bins', type=int, nargs='+')
-
+        cgroup.add_argument('--iter', required=False, help="Number of iterations to calculate the distribution over",
+            metavar="iterations", type=int, nargs=1, default=1000000)
         # check if sum of init/final state bin is <= total # of bins?
         #
         # output option?
     # @ self, args
+
     def process_args(self, args):
         '''Take argparse-processed arguments associated with this component and deal
         with them appropriately (setting instance variables, etc)'''
-        self.data_reader.open(mode='r')
         args = parser.parse_args()
-        #with self.data_reader: # opens HDF5 file?
-            #if args.config_from_file == False:
-                #continue
-                    #self.binning.set_we_h5file_info(self.n_iter,self.data_reader)
-                    #self.binning.process_args(args)
+        self.data_reader.process_args(args) # Q: open hdf5 file for reading?
+        self.input_file = h5py.File(self.data_reader.we_h5filename) # Q: is there another way to do this?
+        if (args.iter):
+            self.iter = args.iter[0]
+        set_bin_info(args)
+        set_K(args)
 
-            #self.output_filename = args.output
-        #set_bin_info()
-        # set up K? (transition matrix)
-
-    def set_bin_info(n_bins, init_bins, target_bins):
-        self.n_bins = n_bins
-        self.init_bins = init_bins
-        self.target_bins = target_bins
+    def set_bin_info(self, args):
+        self.n_bins = args.nbins
+        self.init_bins = args.i
+        self.target_bins = args.f
         # cbins: bins in neither initial nor target state
         self.cbins = list(x for x in range(0, self.n_bins) if x not in self.target_bins)
         self.merged_rates = np.empty(self.n_bins - len(self.target_bins)) # size: num bins which are NOT Target bins
 
-    def go(self):
-        '''Perform the analysis associated with this tool.'''
-        # 1. Get cbins: bins not in
-        # 2.
-        # 3. get transition matrix K
-        # 4. open h5 file
-        # 5. trans_m: empty shell for sparse trans matrix w/weights
-        # 6. fill with flux info
-        dell = []
-        for row in range(self.n_bins):
-            if trans_m[row, :].sum() == 0:
+    def set_K(self, args):
+        f = self.input_file # convenience variable
+        # Empty shell for sparse trans matrix with weights.
+        self.K = np.zeroes((n_bins,n_bins))
+        # Fill matrix with flux vals from h5 file.
+        for iter in f['iterations'].keys():
+            grp = f['iterations'][iter]
+            rows = grp['rows']
+            cols = grp['cols']
+            flux = grp['flux']
+            trans_m[rows, cols] += flux
+        # Normalize flux values
+        for row in range(n_bins):
+            if self.K[row, :].sum() == 0:
                 dell.append(row)
             else:
-                trans_m[row, :] /= trans_m[row, :].sum()
-
+                self.K[row, :] /= trans_m[row, :].sum()
         # NaN values are set to zero, infinity values are set to large finite numbers.
-        K = np.nan_to_num(trans_m)
-        print(K)
+        self.K = np.nan_to_num(self.K)
 
-        # eigenvalues, eigenvectors of transpose of K
+    def go(self):
+        '''Perform the analysis associated with this tool.'''
+        # eigenvalues, eigenvectors of tranpose of K
         eigvals, eigvecs = LA.eig(K.T)
-        unity = (np.abs(np.real(eigvals) - 1)).argmin()  # index of Smallest value from (abs of (real part of eigenvalues - 1))
-        print(eigvals, eigvecs)
-        eq_pop = np.abs(np.real(eigvecs)[unity])  # abs val of real part of eigenvec at index of unity
-        eq_pop /= eq_pop.sum()  # divide by self? what is this
-        # TODO: change this to actual distr_prob
-        # probability of starting in init bin A.
-        distr_prob = np.random.rand(len(self.init_bins))
-        paths = []
+        unity = (np.abs(np.real(eigvals) - 1)).argmin()
+        print("eigenvalues", eigvals)
+        print("eigenvectors", eigvecs)
+        print("unity", unity)
+        eq_pop = np.abs(np.real(eigvecs)[unity])
+        eq_pop /= eq_pop.sum()
 
+        # probability of starting in init bin A.
+        distr_prob = np.random.rand(len(INIT_BINS))
+        paths = []
+        # t_bins: all bins which are not target bins.
+        t_bins = list(x for x in range(0, NBINS) if x not in TARGET_BINS)
         # lower_bound = mfpt - error
         # lower_bound = 121.8
         lower_bound = 116
 
 
+        # What is this? This is never used
         # Hmmm.  What about...
-        p_dist = np.zeros(self.n_bins)
-        p_dist[self.init_bins] = 1.0/float(len(self.init_bins))
-        pp_dist = np.zeros(self.n_bins)
+        p_dist = np.zeros(NBINS)
+        p_dist[INIT_BINS] = 1.0/float(len(INIT_BINS))
+        pp_dist = np.zeros(NBINS)
         p_dist = eq_pop
 
         p_dist = p_dist
-        p_dist = np.zeros((self.n_bins, self.n_bins))
+        p_dist = np.zeros((NBINS,NBINS))
         p_dist = K.copy()
 
         histogram = []
@@ -122,8 +136,8 @@ class WFptd(WESTTool):
         # 100 iterations?
         for i in range(ITER):
             np_dist = np.dot(K,
-                             p_dist - np.diag(np.diag(p_dist)))
-            histogram.append(np_dist[self.init_bins, self.target_bins]*eq_pop[self.cbins].sum())
+                      p_dist - np.diag(np.diag(p_dist)))
+            histogram.append(np_dist[INIT_BINS, TARGET_BINS]*eq_pop[CBINS].sum())
             p_dist = np_dist
 
         dt = 101
@@ -131,7 +145,7 @@ class WFptd(WESTTool):
         print(np.nan_to_num(histogram).shape, len(range(1, ITER+1)))
         print(ITER, (np.average(range(1,ITER+1), weights=np.nan_to_num(histogram)[:,0])/dt))
         print(eq_pop)
-        print(eq_pop[self.cbins].sum())
+        print(eq_pop[CBINS].sum())
 
 if __name__ == '__main__':
     WFptd().main()
