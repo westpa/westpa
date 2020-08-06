a1ca9255d659157b9e1a77e66dc00dbd29cc6e36
John Russo <russojd@.ohsu.edu>
2020-08-06 16:48:16 -0700

    Keep track of starting working directory so generated h5 can be cleared

--- tests/test_tools/test_w_init.py
+++ tests/test_tools/test_w_init.py
@@ -2,24 +2,36 @@ import unittest
 import argparse
 import os
 
-from .common import CommonToolTest
 from .hdiff import H5Diff
 
 from westpa.cli.core.w_init import entry_point
 from unittest import mock
 
 
-class Test_W_Init(unittest.TestCase, CommonToolTest):
-    def test_init(self):
+class Test_W_Init(unittest.TestCase):
+    test_name = 'W_INIT'
+
+    # This is a little kludgey, but in order to set the class attribute starting_path in runTest, I need to
+    #   have an __init__ method or it errors out, despite the fact that I don't actually initialize the
+    #   variable in it
+    def __init__(self, methodName):
+
+        super().__init__(methodName='test_run_w_init')
+
+    def test_run_w_init(self):
         '''Tests initialization of a WESTPA simulation system from a prebuilt .cfg'''
+
+        self.starting_path = os.getcwd()
+
+        os.chdir(os.path.dirname(__file__) + '/odld')
 
         with mock.patch(
             target='argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(
                 force=True,
-                rcfile='west.cfg',
+                rcfile=os.path.dirname(__file__) + '/odld/west.cfg',
                 bstate_file=None,
-                verbosity=None,
+                verbosity=1,
                 bstates=['initial,1.0'],
                 tstate_file=None,
                 tstates=None,
@@ -34,10 +46,9 @@ class Test_W_Init(unittest.TestCase, CommonToolTest):
         #   to ensure that w_init is producing the same output.
         # Instead, use my H5Diff class.
         # If the checked contents differ, an AssertionError will be raised.
-        diff = H5Diff('west_ref.h5', 'west.h5')
+        diff = H5Diff(os.path.dirname(__file__) + '/odld/west_ref.h5', self.starting_path + '/west.h5')
         diff.check()
 
     def tearDown(self):
 
-        # Don't need this any more
-        os.remove('west.h5')
+        os.remove(self.starting_path + '/west.h5')
