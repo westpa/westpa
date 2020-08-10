863d6a5d6f305cd3951806aaee0351bc1d2e4538
Darian Yang <dty7@pitt.edu>
2020-08-10 17:30:35 -0400

    moved ref directory and included pre-commit formatting.

--- tests/test_tools/test_w_succ.py
+++ tests/test_tools/test_w_succ.py
@@ -1,12 +1,15 @@
-import os, shutil, tempfile
+import os
+import shutil
+import tempfile
 import filecmp
 import unittest
-import h5py
-#from westpa.cli.core.w_succ import find_successful_trajs
+
+# from westpa.cli.core.w_succ import find_successful_trajs
+
 
 class Test_W_Succ(unittest.TestCase):
     '''
-    Class to test that w_succ is working successfully to find and output recycled trajectories. 
+    Class to test that w_succ is working successfully to find and output recycled trajectories.
     '''
 
     test_name = 'W_SUCC'
@@ -16,29 +19,27 @@ class Test_W_Succ(unittest.TestCase):
         First, test if the output contains the correct information of segments which successfully reach a target state.
         Then test for correct file output with -o and --output flags.
         '''
-       
-        ref_dir = os.path.join(os.path.dirname(os.getcwd()), "test_references") 
-        w_succ_ref = os.path.join(ref_dir, "w_succ_ref") 
-        temp = tempfile.TemporaryDirectory(dir = os.getcwd())
+
+        ref_dir = os.path.join(os.getcwd(), "refs")
+        w_succ_ref = os.path.join(ref_dir, "w_succ_ref")
+        temp = tempfile.TemporaryDirectory(dir=os.getcwd())
 
         os.chdir(temp.name)
         shutil.copy2(os.path.join(ref_dir, "west.cfg"), temp.name)
         shutil.copy2(os.path.join(ref_dir, "west.h5"), temp.name)
-        
+
         # generate w_succ command line output file and standardize it by removing WEST data dir header line
         os.system("w_succ > temp.txt")
         os.system("sed -i'.original' '/#/,$!d' temp.txt")
-        
+
         self.assertTrue(filecmp.cmp("temp.txt", w_succ_ref), "the cli output was not successfully generated")
 
         # test output to file args
         os.system("w_succ --output temp_test_output.txt")
-        self.assertTrue(filecmp.cmp("temp_test_output.txt", w_succ_ref), \
-        "--output flag output file was not successfully generated")
+        self.assertTrue(filecmp.cmp("temp_test_output.txt", w_succ_ref), "--output flag output file was not successfully generated")
 
         os.system("w_succ -o temp_test_o.txt")
-        self.assertTrue(filecmp.cmp("temp_test_o.txt", w_succ_ref), \
-        "-o flag output file was not successfully generated")
+        self.assertTrue(filecmp.cmp("temp_test_o.txt", w_succ_ref), "-o flag output file was not successfully generated")
 
         os.chdir(os.path.dirname(os.getcwd()))
         temp.cleanup()
