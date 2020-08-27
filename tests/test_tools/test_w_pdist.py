e09169546c9ba6a05209d0bf246713ab9f99eece
Jeremy Leung <63817169+jeremyleung521@users.noreply.github.com>
2020-08-27 14:43:29 -0400

    Update tests/test_tools/test_w_pdist.py
    Co-authored-by: Joshua Adelman <synapticarbors@users.noreply.github.com>

--- tests/test_tools/test_w_pdist.py
+++ tests/test_tools/test_w_pdist.py
@@ -17,7 +17,7 @@ class Test_W_PDIST(unittest.TestCase):
         shutil.copy2(os.path.join(ref_dir, 'west.cfg'), './')
         shutil.copy2(os.path.join(ref_dir, 'west.h5'), './')
 
-        entrypoint()
+        entry_point()
 
         assert os.path.isfile('./pdist.h5'), "The pdist.h5 file was not generated."
         diff = H5Diff(os.path.join(ref_dir, 'pdist_ref.h5'), './pdist.h5')
