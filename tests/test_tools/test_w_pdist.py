9b1286e1c184919d036a5b9c16ca68b2b013d46a
Jeremy Leung <jml230@pitt.edu>
2020-08-27 16:49:43 -0400

    final working test_w_pdist.py using entry_points and mock.patch

--- tests/test_tools/test_w_pdist.py
+++ tests/test_tools/test_w_pdist.py
@@ -1,8 +1,11 @@
 import os
 import shutil
-from .h5diff import H5Diff
 import unittest
+from unittest import mock
+
+from .h5diff import H5Diff
 from westpa.cli.tools.w_pdist import entry_point
+import argparse
 
 
 class Test_W_PDIST(unittest.TestCase):
@@ -17,7 +20,28 @@ class Test_W_PDIST(unittest.TestCase):
         shutil.copy2(os.path.join(ref_dir, 'west.cfg'), './')
         shutil.copy2(os.path.join(ref_dir, 'west.h5'), './')
 
-        entry_point()
+        with mock.patch(
+            target='argparse.ArgumentParser.parse_args',
+            return_value=argparse.Namespace(
+                verbosity='0',
+                rcfile='west.cfg',
+                max_queue_length=None,
+                we_h5filename='west.h5',
+                construct_dataset=None,
+                dsspecs=None,
+                group_name='pcoord',
+                first_iter=None,
+                last_iter=None,
+                override_iter_start=True,
+                override_iter_stop=True,
+                default_iter_step=None,
+                bins='100',
+                output='pdist.h5',
+                ignore_out_of_range=False,
+                compress=False,
+            ),
+        ):
+            entry_point()
 
         assert os.path.isfile('./pdist.h5'), "The pdist.h5 file was not generated."
         diff = H5Diff(os.path.join(ref_dir, 'pdist_ref.h5'), './pdist.h5')
@@ -26,3 +50,4 @@ class Test_W_PDIST(unittest.TestCase):
         # clean up
         os.remove('west.h5')
         os.remove('pdist.h5')
+        os.remove('west.cfg')
