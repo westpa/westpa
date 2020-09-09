563d14b3ef3042d914bb50fcd1d1995ce5fb50a9
Darian Yang <dty7@pitt.edu>
2020-09-09 14:55:21 -0400

    updated mock patch west.h5 dir

--- tests/test_tools/test_w_succ.py
+++ tests/test_tools/test_w_succ.py
@@ -18,13 +18,13 @@ class Test_W_Succ(unittest.TestCase):
 
     def test_run_w_succ(self):
 
-        ref_dir = os.path.join(os.path.dirname(__file__), "refs")
-        w_succ_ref = os.path.join(ref_dir, "w_succ_ref")
+        ref_dir = os.path.join(os.path.dirname(__file__), 'refs')
+        w_succ_ref = os.path.join(ref_dir, 'w_succ_ref')
         temp = tempfile.TemporaryDirectory(dir=os.path.dirname(__file__))
 
         os.chdir(temp.name)
-        shutil.copy2(os.path.join(ref_dir, "west.cfg"), temp.name)
-        shutil.copy2(os.path.join(ref_dir, "west.h5"), temp.name)
+        shutil.copy2(os.path.join(ref_dir, 'west.cfg'), temp.name)
+        shutil.copy2(os.path.join(ref_dir, 'west.h5'), temp.name)
 
         temp_w_succ_output_file = open('w_succ_temp', 'w+')
 
@@ -32,10 +32,10 @@ class Test_W_Succ(unittest.TestCase):
             target='argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(
                 verbosity='debug',
-                rcfile='west.cfg',
+                rcfile='./west.cfg',
                 output_file=temp_w_succ_output_file,
-                west_h5name='west.h5',
-                anal_h5name='analysis.h5',
+                west_h5name='./west.h5',
+                anal_h5name='./analysis.h5',
             ),
         ):
             entry_point()
