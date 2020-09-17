671cf06ebf93c74f8449bddb10e216dcc95e73a2
Jeremy Leung <63817169+jeremyleung521@users.noreply.github.com>
2020-09-17 19:23:30 -0400

    patchwork implementation of fixtures into test_w_pdist.py

---
+++ tests/test_tools/conftest.py
@@ -0,0 +1,115 @@
+import pytest
+import os
+
+from shutil import copyfile
+
+from westpa import rc
+
+REFERENCE_PATH = os.path.join(os.path.dirname(__file__), 'ref')
+ref_dir = os.path.join(os.path.dirname(__file__), 'refs')
+
+
+H5_FILENAME = 'west.h5'
+CFG_FILENAME = 'west.cfg'
+REF_CFG_FILENAME = 'west_init_ref.cfg'
+ref_cfg_file = 'west_ref.cfg'
+ref_h5_file = 'west_ref.h5'
+
+# TODO: Is putting this here, outside of any function bad practice?
+#   Need it here so that clear_state doesn't take an argument...
+STARTING_PATH = os.getcwd()
+
+@pytest.fixture
+def ref_50iter(request):
+    """
+    Fixture that prepares a simulation directory with a completed 3-iteration WESTPA,
+    west.h5, plus the config file west.cfg
+    """
+
+    os.chdir(ref_dir)
+
+    copyfile(ref_h5_file, H5_FILENAME)
+    copyfile(ref_cfg_file, CFG_FILENAME)
+
+    request.cls.cfg_filepath = os.path.join(ref_dir, CFG_FILENAME)
+    request.cls.h5_filepath = os.path.join(ref_dir, H5_FILENAME)
+
+    os.environ['WEST_SIM_ROOT'] = ref_dir
+
+    request.addfinalizer(clear_state)
+
+
+@pytest.fixture
+def ref_3iter(request):
+    """
+    Fixture that prepares a simulation directory with a completed 3-iteration WESTPA,
+    west.h5, plus the config file west.cfg
+    """
+
+    os.chdir(REFERENCE_PATH)
+
+    copyfile('west_3iter.h5', H5_FILENAME)
+    copyfile(REF_CFG_FILENAME, CFG_FILENAME)
+
+    request.cls.cfg_filepath = os.path.join(REFERENCE_PATH, CFG_FILENAME)
+    request.cls.h5_filepath = os.path.join(REFERENCE_PATH, H5_FILENAME)
+
+    os.environ['WEST_SIM_ROOT'] = REFERENCE_PATH
+
+    request.addfinalizer(clear_state)
+
+
+@pytest.fixture
+def ref_cfg(request):
+    """
+    Fixture that prepares a simulation directory with a populated west.cfg file.
+    """
+
+    os.chdir(REFERENCE_PATH)
+
+    copyfile(REF_CFG_FILENAME, CFG_FILENAME)
+
+    request.cls.cfg_filepath = os.path.join(REFERENCE_PATH, CFG_FILENAME)
+    request.cls.h5_filepath = os.path.join(REFERENCE_PATH, H5_FILENAME)
+    request.cls.ref_h5_filepath = os.path.join(REFERENCE_PATH, 'west_ref.h5')
+
+    os.environ['WEST_SIM_ROOT'] = REFERENCE_PATH
+
+    request.addfinalizer(clear_state)
+
+
+@pytest.fixture
+def ref_initialized(request):
+    """
+    Fixture that prepares a simulation directory with an initialized WESTPA system,
+    west.h5, plus the config file west.cfg
+    """
+
+    os.chdir(REFERENCE_PATH)
+
+    copyfile('west_ref.h5', H5_FILENAME)
+    copyfile(REF_CFG_FILENAME, CFG_FILENAME)
+
+    request.cls.cfg_filepath = os.path.join(REFERENCE_PATH, CFG_FILENAME)
+    request.cls.h5_filepath = os.path.join(REFERENCE_PATH, H5_FILENAME)
+    request.cls.REFERENCE_PATH = REFERENCE_PATH
+
+    os.environ['WEST_SIM_ROOT'] = REFERENCE_PATH
+
+    request.addfinalizer(clear_state)
+
+
+def clear_state():
+
+    os.remove(CFG_FILENAME)
+    os.remove(H5_FILENAME)
+
+    os.chdir(STARTING_PATH)
+
+    os.environ['WEST_SIM_ROOT'] = ''
+
+    rc._sim_manager = None
+    rc._system = None
+    rc._data_manager = None
+    rc._we_driver = None
+    rc._propagator = None
--- tests/test_tools/test_w_pdist.py
+++ tests/test_tools/test_w_pdist.py
@@ -1,32 +1,25 @@
 import os
-import shutil
-import unittest
 from unittest import mock
-
 from h5diff import H5Diff
 from westpa.cli.tools.w_pdist import entry_point
 import argparse
 
 
-class Test_W_PDIST(unittest.TestCase):
+class Test_W_PDIST:
     '''Class to test w_pdist works to generate a file and that it is the same as the sample pdist.h5 file.'''
 
-    test_name = 'W_PDIST'
-
-    def test_run_w_pdist(self):
+    def test_run_w_pdist(self, ref_50iter):
         '''Testing if w_pdist runs as expected and the pdist.h5 file looks good.'''
 
         ref_dir = os.path.join(os.path.dirname(__file__), 'refs')
-        shutil.copy2(os.path.join(ref_dir, 'west.cfg'), './')
-        shutil.copy2(os.path.join(ref_dir, 'west.h5'), './')
 
         with mock.patch(
             target='argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(
                 verbosity='0',
-                rcfile='west.cfg',
+                rcfile=self.cfg_filepath,
                 max_queue_length=None,
-                we_h5filename='west.h5',
+                we_h5filename=self.h5_filepath,
                 construct_dataset=None,
                 dsspecs=None,
                 group_name='pcoord',
@@ -47,6 +40,4 @@ class Test_W_PDIST(unittest.TestCase):
         diff.check()
 
         # clean up
-        os.remove('west.h5')
         os.remove('pdist.h5')
-        os.remove('west.cfg')
