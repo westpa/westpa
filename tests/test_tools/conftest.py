15fc0f90eb2d28c27e970f0fa831a0364e9177ab
jml230 <jml230@pitt.edu>
2020-09-21 15:29:11 -0400

    removed unused code in conftest.py and ref folder for easier merging later on

--- tests/test_tools/conftest.py
+++ tests/test_tools/conftest.py
@@ -5,19 +5,18 @@ from shutil import copyfile
 
 from westpa import rc
 
-REFERENCE_PATH = os.path.join(os.path.dirname(__file__), 'ref')
 refs_dir = os.path.join(os.path.dirname(__file__), 'refs')
 
 
 H5_FILENAME = 'west.h5'
 CFG_FILENAME = 'west.cfg'
-REF_CFG_FILENAME = 'west_init_ref.cfg'
 refs_cfg_file = 'west_ref.cfg'
 refs_h5_file = 'west_ref.h5'
 
 # TODO: Is putting this here, outside of any function bad practice?
 #   Need it here so that clear_state doesn't take an argument...
 STARTING_PATH = os.getcwd()
+
 
 @pytest.fixture
 def ref_50iter(request):
@@ -35,66 +34,6 @@ def ref_50iter(request):
     request.cls.h5_filepath = os.path.join(refs_dir, H5_FILENAME)
 
     os.environ['WEST_SIM_ROOT'] = refs_dir
-
-    request.addfinalizer(clear_state)
-
-
-@pytest.fixture
-def ref_3iter(request):
-    """
-    Fixture that prepares a simulation directory with a completed 3-iteration WESTPA,
-    west.h5, plus the config file west.cfg
-    """
-
-    os.chdir(REFERENCE_PATH)
-
-    copyfile('west_3iter.h5', H5_FILENAME)
-    copyfile(REF_CFG_FILENAME, CFG_FILENAME)
-
-    request.cls.cfg_filepath = os.path.join(REFERENCE_PATH, CFG_FILENAME)
-    request.cls.h5_filepath = os.path.join(REFERENCE_PATH, H5_FILENAME)
-
-    os.environ['WEST_SIM_ROOT'] = REFERENCE_PATH
-
-    request.addfinalizer(clear_state)
-
-
-@pytest.fixture
-def ref_cfg(request):
-    """
-    Fixture that prepares a simulation directory with a populated west.cfg file.
-    """
-
-    os.chdir(REFERENCE_PATH)
-
-    copyfile(REF_CFG_FILENAME, CFG_FILENAME)
-
-    request.cls.cfg_filepath = os.path.join(REFERENCE_PATH, CFG_FILENAME)
-    request.cls.h5_filepath = os.path.join(REFERENCE_PATH, H5_FILENAME)
-    request.cls.ref_h5_filepath = os.path.join(REFERENCE_PATH, 'west_ref.h5')
-
-    os.environ['WEST_SIM_ROOT'] = REFERENCE_PATH
-
-    request.addfinalizer(clear_state)
-
-
-@pytest.fixture
-def ref_initialized(request):
-    """
-    Fixture that prepares a simulation directory with an initialized WESTPA system,
-    west.h5, plus the config file west.cfg
-    """
-
-    os.chdir(REFERENCE_PATH)
-
-    copyfile('west_ref.h5', H5_FILENAME)
-    copyfile(REF_CFG_FILENAME, CFG_FILENAME)
-
-    request.cls.cfg_filepath = os.path.join(REFERENCE_PATH, CFG_FILENAME)
-    request.cls.h5_filepath = os.path.join(REFERENCE_PATH, H5_FILENAME)
-    request.cls.REFERENCE_PATH = REFERENCE_PATH
-
-    os.environ['WEST_SIM_ROOT'] = REFERENCE_PATH
 
     request.addfinalizer(clear_state)
 
