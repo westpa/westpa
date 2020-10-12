f406ad7e30dc3b89f3e2fc1b67923cae141dc181
Anthony Bogetti <anthony.bogetti@gmail.com>
2020-10-12 11:42:32 -0400

    updated w_assign test

--- tests/test_tools/test_w_assign.py
+++ tests/test_tools/test_w_assign.py
@@ -1,24 +1,56 @@
+import unittest
+import argparse
 import os
 import shutil
-from .h5diff import H5Diff
-import unittest
+
+from h5diff import H5Diff
+
+from westpa.cli.tools.w_assign import entry_point
+from unittest import mock
 
 
 class Test_W_Assign(unittest.TestCase):
 
     test_name = 'W_ASSIGN'
 
-    def test_run_w_assign(self):
-        '''Testing if w_assign runs as expected and the assign.h5 file looks good.'''
+    def __init__(self, methodName):
 
-        ref_dir = os.path.join(os.path.dirname(__file__), 'refs')
-        shutil.copy2(os.path.join(ref_dir, 'west.cfg'), './')
-        shutil.copy2(os.path.join(ref_dir, 'west.h5'), './')
-        os.system('w_assign -W ./west.h5 --config-from-file --scheme TEST')
-        assert os.path.isfile('./ANALYSIS/TEST/assign.h5'), "The assign.h5 file was not generated."
-        diff = H5Diff(os.path.join(ref_dir, 'assign_ref.h5'), './ANALYSIS/TEST/assign.h5')
+        super().__init__(methodName='test_run_w_assign')
+
+    def setUp(self):
+
+        self.starting_path = os.getcwd()
+
+        self.ref_path = os.path.dirname(__file__) + '/ref'
+        os.environ['WEST_SIM_ROOT'] = self.ref_path
+
+        os.chdir(self.ref_path)
+
+    def test_run_w_assign(self):
+
+        with mock.patch(
+            target='argparse.ArgumentParser.parse_args',
+            return_value=argparse.Namespace(
+                verbosity='debug',
+                rcfile='west.cfg',
+                max_queue_length=None,
+                we_h5filename='west.h5',
+                construct_dataset=None,
+                dsspecs=None,
+                output='assign.h5',
+                subsample=None,
+                config_from_file=True,
+                scheme='TEST',
+            ),
+        ):
+
+            entry_point()
+
+        diff = H5Diff(self.ref_path + '/assign_ref.h5', self.ref_path + '/ANALYSIS/TEST/assign.h5')
         diff.check()
 
+    def tearDown(self):
+
+        os.environ['WEST_SIM_ROOT'] = ''
         shutil.rmtree('ANALYSIS')
-        os.remove('west.h5')
-        os.remove('west.cfg')
+        os.chdir(self.starting_path)
