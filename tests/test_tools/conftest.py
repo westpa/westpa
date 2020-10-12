4c7008907320bdb2d887e1944cb83e48b847e926
Darian Yang <dty7@pitt.edu>
2020-10-12 13:25:36 -0400

    updated w_succ test to be compliant with pytest fixture formatting

---
+++ tests/test_tools/conftest.py
@@ -0,0 +1,53 @@
+import pytest
+import os
+
+from shutil import copyfile
+from westpa import rc
+
+REFERENCE_PATH = os.path.join(os.path.dirname(__file__), 'refs')
+
+H5_FILENAME = 'west.h5'
+CFG_FILENAME = 'west.cfg'
+refs_cfg_file = 'west_ref.cfg'
+refs_h5_file = 'west_ref.h5'
+
+# TODO: Is putting this here, outside of any function bad practice?
+#   Need it here so that clear_state doesn't take an argument...
+STARTING_PATH = os.getcwd()
+
+
+@pytest.fixture
+def ref_initialized(request):
+    """
+    Fixture that prepares a simulation directory with an initialized WESTPA system,
+    west.h5, plus the config file west.cfg
+    """
+
+    os.chdir(REFERENCE_PATH)
+
+    copyfile(refs_h5_file, H5_FILENAME)
+    copyfile(refs_cfg_file, CFG_FILENAME)
+
+    request.cls.cfg_filepath = os.path.join(REFERENCE_PATH, CFG_FILENAME)
+    request.cls.h5_filepath = os.path.join(REFERENCE_PATH, H5_FILENAME)
+    request.cls.REFERENCE_PATH = REFERENCE_PATH
+
+    os.environ['WEST_SIM_ROOT'] = REFERENCE_PATH
+
+    request.addfinalizer(clear_state)
+
+
+def clear_state():
+
+    os.remove(CFG_FILENAME)
+    os.remove(H5_FILENAME)
+
+    os.chdir(STARTING_PATH)
+
+    os.environ['WEST_SIM_ROOT'] = ''
+
+    rc._sim_manager = None
+    rc._system = None
+    rc._data_manager = None
+    rc._we_driver = None
+    rc._propagator = None
--- tests/test_tools/test_w_succ.py
+++ tests/test_tools/test_w_succ.py
@@ -1,41 +1,29 @@
 import os
 import filecmp
-import shutil
-import tempfile
-import unittest
 import argparse
 
 from westpa.cli.core.w_succ import entry_point
 from unittest import mock
 
 
-class Test_W_Succ(unittest.TestCase):
+class Test_W_Succ:
     '''
     Class to test that w_succ is working successfully to find and output recycled trajectories.
     '''
 
-    test_name = 'W_SUCC'
+    def test_run_w_succ(self, ref_initialized):
 
-    def test_run_w_succ(self):
-
-        ref_dir = os.path.join(os.path.dirname(__file__), 'refs')
-        w_succ_ref = os.path.join(ref_dir, 'w_succ_ref')
-        temp = tempfile.TemporaryDirectory(dir=os.path.dirname(__file__))
-
-        os.chdir(temp.name)
-        shutil.copy2(os.path.join(ref_dir, 'west.cfg'), temp.name)
-        shutil.copy2(os.path.join(ref_dir, 'west.h5'), temp.name)
-
-        temp_w_succ_output_file = open('w_succ_temp', 'w+')
+        w_succ_ref = os.path.join(self.REFERENCE_PATH, 'w_succ_ref')
+        temp_w_succ_output_file = open('w_succ_temp', 'w')
 
         with mock.patch(
             target='argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(
                 verbosity='debug',
-                rcfile='./west.cfg',
+                rcfile=self.cfg_filepath,
                 output_file=temp_w_succ_output_file,
-                west_h5name='./west.h5',
-                anal_h5name='./analysis.h5',
+                west_h5name=self.h5_filepath,
+                anal_h5name='analysis.h5',
             ),
         ):
             entry_point()
@@ -43,7 +31,6 @@ class Test_W_Succ(unittest.TestCase):
         temp_w_succ_output_file.close()
 
         assert os.path.isfile('w_succ_temp'), 'The output file was not generated.'
-        self.assertTrue(filecmp.cmp(w_succ_ref, 'w_succ_temp'), 'The output file was not generated correctly')
+        assert filecmp.cmp(w_succ_ref, 'w_succ_temp'), 'The output file was not generated correctly'
 
-        os.chdir(os.path.dirname(temp.name))
-        temp.cleanup()
+        os.remove('w_succ_temp')
