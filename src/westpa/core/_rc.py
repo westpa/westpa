947822212855415cf1fb7c60417598c866be4b44
Kim F. Wong <kimwong@pitt.edu>
2020-11-30 16:33:24 -0500

    Modifications to enable binless strategies using an external function.

--- src/westpa/core/_rc.py
+++ src/westpa/core/_rc.py
@@ -296,6 +296,23 @@ class WESTRC:
         else:
             we_driver = extloader.get_object(drivername)(rc=self)
         log.debug('loaded WE algorithm driver: {!r}'.format(we_driver))
+
+        group_function = self.config.get(['west', 'drivers', 'group_function'], 'default')
+        if group_function.lower() == 'default':
+            try:
+                group_function = 'westpa.core.we_driver._group_walkers_identity'
+                we_driver.group_function = westpa.core.we_driver._group_walkers_identity
+            except Exception:
+                pass
+        else:
+            we_driver.group_function = extloader.get_object(group_function)
+        we_driver.group_function_kwargs = self.config.get(['west', 'drivers', 'group_arguments'])
+        # Necessary if the user hasn't specified any options.
+        if we_driver.group_function_kwargs is None:
+            we_driver.group_function_kwargs = {}
+        log.debug('loaded WE algorithm driver grouping function {!r}'.format(group_function))
+        log.debug('WE algorithm driver grouping function kwargs: {!r}'.format(we_driver.group_function_kwargs))
+
         return we_driver
 
     def get_we_driver(self):
--- src/westpa/core/propagators/executable.py
+++ src/westpa/core/propagators/executable.py
@@ -124,7 +124,7 @@ class ExecutablePropagator(WESTPropagator):
         self.addtl_child_environ.update({k: str(v) for k, v in (config['west', 'executable', 'environ'] or {}).items()})
 
         # Load configuration items relating to child processes
-        for child_type in ('propagator', 'pre_iteration', 'post_iteration', 'get_pcoord', 'gen_istate'):
+        for child_type in ('propagator', 'pre_iteration', 'post_iteration', 'get_pcoord', 'gen_istate', 'group_walkers'):
             child_info = config.get(['west', 'executable', child_type])
             if not child_info:
                 continue
