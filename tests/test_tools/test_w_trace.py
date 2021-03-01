d2f4752a3b6bec8947c304c50404eefecf83d45f
jml230 <jml230@pitt.edu>
2021-03-01 15:29:51 -0500

    moved test_data_manager and fixed test_w_trace

--- tests/test_tools/test_w_trace.py
+++ tests/test_tools/test_w_trace.py
@@ -4,15 +4,18 @@ import h5py
 
 from common import CommonToolTest
 from westpa.cli.tools.w_trace import WTraceTool
+from h5diff import H5Diff
 
 
 class Test_W_Trace_Args(unittest.TestCase, CommonToolTest):
     '''Class to test w_trace works with different positional arguments (of the form n_iter:seg_id [n_iter:seg_id])
-    This class tests that a) w_trace works with different numbers of positional arguments, and b) that the output file is set up
+    This class tests that a) w_trace works with different numbers of positional arguments, b) that the output file is set up
     correctly AND the data from subsequent traces is appended (in other words, if there is an existing output file, it
-    is not overwritten each time trace is called)'''
+    is not overwritten each time trace is called), and c) that the content of the h5 files generated match the content of previously
+    generated reference files.'''
 
     arg_combos = [['20:0'], ['20:1', '20:2']]
+    ref_files = ['refs/trajs_ref_20_0.h5', 'refs/trajs_ref_full.h5']
 
     test_name = 'W_TRACE'
 
@@ -24,7 +27,7 @@ class Test_W_Trace_Args(unittest.TestCase, CommonToolTest):
         self.outfile = self.mktemp(prefix='trace')
         outarg = '-o={}'.format(self.outfile)
 
-        for args in self.arg_combos:
+        for idx, args in enumerate(self.arg_combos):
             self.w = WTraceTool()
 
             self.args_so_far.extend(args)
@@ -33,7 +36,7 @@ class Test_W_Trace_Args(unittest.TestCase, CommonToolTest):
             self.endpoints.extend(self.w.endpoints)
             args.remove(outarg)
 
-            test_outcome, err = self.check_runs_with_args()
+            test_outcome, err = self.check_runs_with_args(idx)
 
             yield self.check_args, test_outcome, err, args
 
@@ -41,7 +44,7 @@ class Test_W_Trace_Args(unittest.TestCase, CommonToolTest):
 
         yield self.check_args, test_outcome, err, args
 
-    def check_output(self):
+    def check_output(self, arg_idx):
 
         with h5py.File(self.outfile) as f:
 
@@ -56,3 +59,6 @@ class Test_W_Trace_Args(unittest.TestCase, CommonToolTest):
             assert list(traj_group.keys()) == expected_groups, "H5 groups ({}) are not as expected ({})".format(
                 list(traj_group.keys()), expected_groups
             )
+
+        diff = H5Diff(self.ref_files[arg_idx], self.outfile)
+        diff.check()
