e1510d41340144655d35c475ebf781e931888053
John Russo <russojd@.ohsu.edu>
2021-03-23 16:49:37 -0700

    Refactored simulation logic out of w_run.entry_point() and updated test

--- tests/test_tools/test_w_run.py
+++ tests/test_tools/test_w_run.py
@@ -1,39 +1,80 @@
-import argparse
-
 import h5py
 
-from westpa.cli.core.w_run import entry_point
-from unittest import mock
+import westpa.cli.core.w_run
+import westpa.work_managers as work_managers
 
 
 class Test_W_Run:
     def test_run_w_run(self, ref_initialized):
         '''Tests running an initialized WESTPA system for 3 iterations'''
 
-        with mock.patch(
-            target='argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(
-                n_workers=None,
-                only_one_segment=False,
-                rcfile=self.cfg_filepath,
-                verbosity='0',
-                work_manager=None,
-                zmq_comm_mode=None,
-                zmq_downstream_ann_endpoint=None,
-                zmq_downstream_rr_endpoint=None,
-                zmq_master_heartbeat=None,
-                zmq_mode=None,
-                zmq_read_host_info=None,
-                zmq_shutdown_timeout=None,
-                zmq_startup_timeout=None,
-                zmq_timeout_factor=None,
-                zmq_upstream_ann_endpoint=None,
-                zmq_upstream_rr_endpoint=None,
-                zmq_worker_heartbeat=None,
-                zmq_write_host_info=None,
-            ),
-        ):
-            entry_point()
+        class TestArgs:
+            def __init__(
+                self,
+                n_workers,
+                only_one_segment,
+                rcfile,
+                verbosity,
+                work_manager,
+                zmq_comm_mode,
+                zmq_downstream_ann_endpoint,
+                zmq_downstream_rr_endpoint,
+                zmq_master_heartbeat,
+                zmq_mode,
+                zmq_read_host_info,
+                zmq_shutdown_timeout,
+                zmq_startup_timeout,
+                zmq_timeout_factor,
+                zmq_upstream_ann_endpoint,
+                zmq_upstream_rr_endpoint,
+                zmq_worker_heartbeat,
+                zmq_write_host_info,
+            ):
+
+                self.n_workers = n_workers
+                self.only_one_segment = only_one_segment
+                self.rcfile = rcfile
+                self.verbosity = verbosity
+                self.work_manager = work_manager
+                self.zmq_comm_mode = zmq_comm_mode
+                self.zmq_downstream_ann_endpoint = zmq_downstream_ann_endpoint
+                self.zmq_downstream_rr_endpoint = zmq_downstream_rr_endpoint
+                self.zmq_master_heartbeat = zmq_master_heartbeat
+                self.zmq_mode = zmq_mode
+                self.zmq_read_host_info = zmq_read_host_info
+                self.zmq_shutdown_timeout = zmq_shutdown_timeout
+                self.zmq_startup_timeout = zmq_startup_timeout
+                self.zmq_timeout_factor = zmq_timeout_factor
+                self.zmq_upstream_ann_endpoint = zmq_upstream_ann_endpoint
+                self.zmq_upstream_rr_endpoint = zmq_upstream_rr_endpoint
+                self.zmq_worker_heartbeat = zmq_worker_heartbeat
+                self.zmq_write_host_info = zmq_write_host_info
+
+        args = TestArgs(
+            n_workers=None,
+            only_one_segment=False,
+            rcfile=self.cfg_filepath,
+            verbosity='0',
+            work_manager=None,
+            zmq_comm_mode=None,
+            zmq_downstream_ann_endpoint=None,
+            zmq_downstream_rr_endpoint=None,
+            zmq_master_heartbeat=None,
+            zmq_mode=None,
+            zmq_read_host_info=None,
+            zmq_shutdown_timeout=None,
+            zmq_startup_timeout=None,
+            zmq_timeout_factor=None,
+            zmq_upstream_ann_endpoint=None,
+            zmq_upstream_rr_endpoint=None,
+            zmq_worker_heartbeat=None,
+            zmq_write_host_info=None,
+        )
+
+        westpa.rc.process_args(args)
+        work_managers.environment.process_wm_args(args)
+
+        westpa.cli.core.w_run.run_simulation()
 
         # Since the dynamics produce slightly different output in each iteration, just check that
         #    the third iteration exists in the h5 file.
