3693b74d871d006f1c0a9f453060388bacde0ee9
John Russo <russojd@.ohsu.edu>
2021-03-23 16:52:58 -0700

    Updated TestArgs object to auto-populate attributes from kwargs

--- tests/test_tools/test_w_run.py
+++ tests/test_tools/test_w_run.py
@@ -4,53 +4,17 @@ import westpa.cli.core.w_run
 import westpa.work_managers as work_managers
 
 
+# Placeholder class that will set all kwargs as attributes
+class MockArgs:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
 class Test_W_Run:
     def test_run_w_run(self, ref_initialized):
         '''Tests running an initialized WESTPA system for 3 iterations'''
 
-        class TestArgs:
-            def __init__(
-                self,
-                n_workers,
-                only_one_segment,
-                rcfile,
-                verbosity,
-                work_manager,
-                zmq_comm_mode,
-                zmq_downstream_ann_endpoint,
-                zmq_downstream_rr_endpoint,
-                zmq_master_heartbeat,
-                zmq_mode,
-                zmq_read_host_info,
-                zmq_shutdown_timeout,
-                zmq_startup_timeout,
-                zmq_timeout_factor,
-                zmq_upstream_ann_endpoint,
-                zmq_upstream_rr_endpoint,
-                zmq_worker_heartbeat,
-                zmq_write_host_info,
-            ):
-
-                self.n_workers = n_workers
-                self.only_one_segment = only_one_segment
-                self.rcfile = rcfile
-                self.verbosity = verbosity
-                self.work_manager = work_manager
-                self.zmq_comm_mode = zmq_comm_mode
-                self.zmq_downstream_ann_endpoint = zmq_downstream_ann_endpoint
-                self.zmq_downstream_rr_endpoint = zmq_downstream_rr_endpoint
-                self.zmq_master_heartbeat = zmq_master_heartbeat
-                self.zmq_mode = zmq_mode
-                self.zmq_read_host_info = zmq_read_host_info
-                self.zmq_shutdown_timeout = zmq_shutdown_timeout
-                self.zmq_startup_timeout = zmq_startup_timeout
-                self.zmq_timeout_factor = zmq_timeout_factor
-                self.zmq_upstream_ann_endpoint = zmq_upstream_ann_endpoint
-                self.zmq_upstream_rr_endpoint = zmq_upstream_rr_endpoint
-                self.zmq_worker_heartbeat = zmq_worker_heartbeat
-                self.zmq_write_host_info = zmq_write_host_info
-
-        args = TestArgs(
+        args = MockArgs(
             n_workers=None,
             only_one_segment=False,
             rcfile=self.cfg_filepath,
