8b5d72d2a72770390987c6f78430d8f6592a96b0
John Russo <russojd@.ohsu.edu>
2021-07-08 16:52:16 -0700

    Updated __init__.py so WESTPA can find restarting plugin

--- src/westpa/westext/__init__.py
+++ src/westpa/westext/__init__.py
@@ -1,6 +1,6 @@
 from . import wess
 from . import weed
 from . import stringmethod
+from . import hamsm_restarting
 
-
-__all__ = ['wess', 'weed', 'stringmethod']
+__all__ = ['wess', 'weed', 'stringmethod', 'hamsm_restarting']
