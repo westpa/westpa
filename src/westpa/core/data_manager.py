8f9b28de454e96a4f2fc807c8a9fd78d731c5274
jml230 <jml230@pitt.edu>
2021-07-19 22:17:55 -0400

    bytestring fix for istates and state labels
    Co-authored-by: JD Russo <russojd@ohsu.edu>

--- src/westpa/core/data_manager.py
+++ src/westpa/core/data_manager.py
@@ -468,7 +468,7 @@ class WESTDataManager:
                 tstate_pcoords = tstate_group['pcoord'][...]
 
                 tstates = [
-                    TargetState(state_id=i, label=str(row['label']), pcoord=pcoord.copy())
+                    TargetState(state_id=i, label=h5io.tostr(row['label']), pcoord=pcoord.copy())
                     for (i, (row, pcoord)) in enumerate(zip(tstate_index, tstate_pcoords))
                 ]
             else:
@@ -532,7 +532,7 @@ class WESTDataManager:
             bstates = [
                 BasisState(
                     state_id=i,
-                    label=row['label'],
+                    label=h5io.tostr(row['label']),
                     probability=row['probability'],
                     auxref=h5io.tostr(row['auxref']) or None,
                     pcoord=pcoord.copy(),
