46cb9c2369a35655b0cf13a213d1dacd49625049
Ali Sinan Saglam <asinansaglam@gmail.com>
2021-08-26 14:49:27 -0400

    uncommenting other plug-ins back in

--- src/westpa/westext/__init__.py
+++ src/westpa/westext/__init__.py
@@ -1,7 +1,6 @@
-# from . import wess
-# from . import weed
-# from . import stringmethod
+from . import wess
+from . import weed
+from . import stringmethod
 from . import adaptvoronoi
 
-# __all__ = ['wess', 'weed', 'stringmethod', 'adaptvoronoi']
-__all__ = ['adaptvoronoi']
+__all__ = ['wess', 'weed', 'stringmethod', 'adaptvoronoi']
