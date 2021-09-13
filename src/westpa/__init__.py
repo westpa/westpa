b92b64ea5d55183b21de3b7466a959619bb8871a
She Zhang <she.zhang@eyesopen.com>
2021-09-13 12:03:24 -0400

    version

--- src/westpa/__init__.py
+++ src/westpa/__init__.py
@@ -9,6 +9,6 @@ __all__ = ['Segment', 'WESTSystem', 'BasisState', 'TargetState', '_rc']
 
 rc = _rc.WESTRC()
 
-__version__ = "2020.3.15"  # get_versions()["version"]
+__version__ = "2021.1.1"  # get_versions()["version"]
 
 del get_versions
