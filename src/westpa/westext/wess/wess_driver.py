72882bb9845c4355dcbc8b28d5b112199bb26ee0
She Zhang <she.zhang@eyesopen.com>
2021-10-05 14:24:31 -0400

    Added an optional parameter to wess driver plugin for assigning custom bins

--- src/westpa/westext/wess/wess_driver.py
+++ src/westpa/westext/wess/wess_driver.py
@@ -7,6 +7,7 @@ import westpa
 from westpa.core.yamlcfg import check_bool
 from westpa.core.kinetics import RateAverager
 from westpa.westext.wess.ProbAdjust import prob_adjust
+from westpa.core._rc import bins_from_yaml_dict
 
 EPS = np.finfo(np.float64).eps
 
@@ -72,6 +73,9 @@ class WESSDriver:
         self.rate_calc_queue_size = plugin_config.get('rate_calc_queue_size', 1)
         self.rate_calc_n_blocks = plugin_config.get('rate_calc_n_blocks', 1)
 
+        bin_dict = plugin_config.get('bins', None)
+        self.bin_mapper = None if bin_dict is None else bins_from_yaml_dict(bin_dict)
+
         if self.do_reweight:
             sim_manager.register_callback(sim_manager.prepare_new_iteration, self.prepare_new_iteration, self.priority)
 
@@ -116,7 +120,7 @@ class WESSDriver:
         else:
             log.debug('reweighting')
 
-        mapper = we_driver.bin_mapper
+        mapper = we_driver.bin_mapper if self.bin_mapper is None else self.bin_mapper
         bins = we_driver.next_iter_binning
         n_bins = len(bins)
         westpa.rc.pstatus('Averaging rates')
