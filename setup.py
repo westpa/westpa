6736cf306e3a6868b1d1e63fbf5434765c0fbbdc
JD Russo <russojd@ohsu.edu>
2021-12-03 11:09:06 -0800

    Remove mdtraj from extras_require

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ INSTALL_REQUIRES = [
 ]
 
 EXTRAS_REQUIRE = {
-    "tests": ["pytest", "pytest-cov", "nose", "mdtraj >= 1.9.5"],
+    "tests": ["pytest", "pytest-cov", "nose"],
     "mpi": ["mpi4py"],
 }
 
