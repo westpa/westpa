f7f3e7a54b6eaeb6bc0b47ca7092c4cfd4884922
She Zhang <she.zhang@eyesopen.com>
2022-02-18 00:43:04 -0700

    a few more OE related tweaks

--- setup.py
+++ setup.py
@@ -109,8 +109,6 @@ CLASSIFIERS = [
 ]
 
 INSTALL_REQUIRES = [
-    "numpy >= 1.16.0",
-    "scipy >= 0.19.1",
     "h5py >= 2.10",
     "mdtraj >= 1.9.5",
     "pyyaml",
@@ -118,10 +116,18 @@ INSTALL_REQUIRES = [
     "matplotlib",
     "blessings",
     "ipykernel",
-    "tqdm",
     "pandas",
     "tables",
 ]
+
+SETUP_REQUIRES = [
+    "numpy>=1.16.0",
+    "scipy>=0.19.1",
+    "Cython>=0.29.16",
+    "tqdm",
+]
+
+INSTALL_REQUIRES += SETUP_REQUIRES
 
 EXTRAS_REQUIRE = {
     "tests": ["pytest", "pytest-cov", "nose"],
@@ -136,7 +142,7 @@ metadata = dict(
     url='http://github.com/westpa/westpa',
     license='MIT',
     long_description=open('README.rst', encoding='utf8').read(),
-    version="2022.1.1a2",  # versioneer.get_version(),
+    version="2022.1.1a3",  # versioneer.get_version(),
     keywords='',
     # cmdclass=versioneer.get_cmdclass(),
     python_requires=">=3.6",
@@ -144,6 +150,7 @@ metadata = dict(
     classifiers=CLASSIFIERS,
     entry_points={'console_scripts': console_scripts},
     install_requires=INSTALL_REQUIRES,
+    setup_requires=SETUP_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
     package_data={},
     packages=find_packages(where='src'),
@@ -152,5 +159,9 @@ metadata = dict(
 
 
 if __name__ == '__main__':
-    metadata['ext_modules'] = extensions()
+    from sys import argv
+
+    if argv[1] != "egg_info":
+        metadata['ext_modules'] = extensions()
+
     setup(**metadata)
