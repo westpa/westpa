32b9c19bad411dfd6b3d89a69bbec3c177de57f4
Anthony Bogetti <anthony.bogetti@gmail.com>
2022-04-06 15:55:47 -0400

    added option to cluster in 2D

--- src/westpa/core/binning/binless.py
+++ src/westpa/core/binning/binless.py
@@ -10,9 +10,10 @@ def map_binless(coords, mask, output, *args, **kwargs):
     evenly spaced bins between the segments with the min and max pcoord values. Extrema and
     bottleneck segments are assigned their own bins.'''
 
-    groups_per_dim = kwargs.get("groups_per_dim")
+    n_groups = kwargs.get("n_groups")
+    n_dims = kwargs.get("n_dims")
     group_function = get_object(kwargs.get("group_function"))
-    ndim = len(groups_per_dim)
+    ndim = n_dims
 
     if not np.any(mask):
         return output
@@ -43,36 +44,39 @@ def map_binless(coords, mask, output, *args, **kwargs):
 
     # filter the list of coordinates (which contains coordinates outside of the binless region)
     # to obtain only the ones we want to cluster
-    # we do this per pcoord dimension
-    for n in range(ndim):
-        binless_coords = coords[mask, n]
-        nsegs_binless = len(binless_coords)
+    # this is done with all dimensions at once
+    binless_coords = coords[mask]
+    nsegs_binless = len(binless_coords)
 
-        # we need to make sure that the number of segments in the binless region is greater than
-        # the number of clusters we request
-        # if only one segment in the binless region, assign it to a single cluster
-        if nsegs_binless == 1:
-            clusters = [0]
-        # if there are more than one segment in the binless region but still the total is less than
-        # our target number, adjust our target number to be the number of segments in the binless
-        # region minus one
-        elif nsegs_binless < groups_per_dim[n]:
-            clusters = group_function(binless_coords, nsegs_binless, splitting)
-        # if there are enough segments in the binless region, proceed as planned
-        elif nsegs_binless >= groups_per_dim[n]:
-            clusters = group_function(binless_coords, groups_per_dim[0], splitting)
+    # we need to make sure that the number of segments in the binless region is greater than
+    # the number of clusters we request
+    # if only one segment in the binless region, assign it to a single cluster
+    if nsegs_binless == 1:
+        clusters = [0]
+    # if there are more than one segment in the binless region but still the total is less than
+    # our target number, adjust our target number to be the number of segments in the binless
+    # region minus one
+    elif nsegs_binless < n_groups:
+        clusters = group_function(binless_coords, nsegs_binless, splitting)
+    # if there are enough segments in the binless region, proceed as planned
+    elif nsegs_binless >= n_groups:
+        clusters = group_function(binless_coords, n_groups, splitting)
 
-        # this is a good place to say this... output is a list which matches the length of allcoords
-        # allcoords is a collection of all initial and final segment coords for that iteration
-        # we first filtered those to only contain the final data points, since those are the ones we care
-        # about clustering
-        # we then filtered to only have the coords in the binless region, since, again, those are what we care about
-        # we then assigned each to a cluster which is essentially a slitting index
-        # all that's left is to find where each binless segment is in the output and insert the cluster index there
-        for idx, val in enumerate(binless_coords):
-            output[allcoords[:, n] == val] = clusters[idx]
+    # this is a good place to say this... output is a list which matches the length of allcoords
+    # allcoords is a collection of all initial and final segment coords for that iteration
+    # we first filtered those to only contain the final data points, since those are the ones we care
+    # about clustering
+    # we then filtered to only have the coords in the binless region, since, again, those are what we care about
+    # we then assigned each to a cluster which is essentially a slitting index
+    # all that's left is to find where each binless segment is in the output and insert the cluster index there
+    for idx, val in enumerate(binless_coords):
+        if ndim > 1:
+            mask2 = np.logical_and(allcoords[:, 0] == val[0], allcoords[:, 1] == val[1])
+        else:
+            mask2 = allcoords[:, 0] == val[0]
+        output[mask2] = clusters[idx]
 
-        return output
+    return output
 
 
 class BinlessMapper(FuncBinMapper):
@@ -80,7 +84,7 @@ class BinlessMapper(FuncBinMapper):
     the progress coordinte. Extrema and bottleneck segments are assigned
     to their own bins.'''
 
-    def __init__(self, ngroups, group_function):
-        kwargs = dict(groups_per_dim=ngroups, group_function=group_function)
-        n_total_groups = np.prod(ngroups)
+    def __init__(self, ngroups, ndims, group_function):
+        kwargs = dict(n_groups=ngroups, n_dims=ndims, group_function=group_function)
+        n_total_groups = ngroups
         super().__init__(map_binless, n_total_groups, kwargs=kwargs)
