d8fa594c1b06be6b8d60223cc0022cc2edfd04dd
jml230 <jml230@pitt.edu>
2022-10-05 10:37:34 -0400

    add longer timeout to zmq test_environment test

--- tests/test_work_managers/test_environment.py
+++ tests/test_work_managers/test_environment.py
@@ -79,6 +79,7 @@ class TestInstantiations(unittest.TestCase):
     def testZeroMQ(self):
         os.environ['WM_WORK_MANAGER'] = 'zmq'
         os.environ['WM_N_WORKERS'] = str(3)
+        os.environ['WM_TIMEOUT_FACTOR'] = str(10)
         work_manager = make_work_manager()
         assert isinstance(work_manager, ZMQWorkManager)
         with work_manager:
