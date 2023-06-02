231e9fc8c0983513687eead1fc612731d34363f5
Jeff Thompson <jeff.thompson@eyesopen.com>
2023-06-02 17:59:25 -0600

    Create initial states after bin assignment.

--- src/westpa/core/sim_manager.py
+++ src/westpa/core/sim_manager.py
@@ -583,66 +583,52 @@ class WESimManager:
         segments = list(self.incomplete_segments.values())
         log.debug('iteration {:d}: propagating {:d} segments'.format(self.n_iter, len(segments)))
 
-        # all futures dispatched for this iteration
-        futures = set()
-        segment_futures = set()
-
-        # Immediately dispatch any necessary initial state generation
-        istate_gen_futures = self.get_istate_futures()
-        futures.update(istate_gen_futures)
-
         # Dispatch propagation tasks using work manager
+        segment_futures = set()
         for segment_block in grouper(self.propagator_block_size, segments):
             segment_block = [_f for _f in segment_block if _f]
             pbstates, pistates = westpa.core.states.pare_basis_initial_states(
                 self.current_iter_bstates, list(self.current_iter_istates.values()), segment_block
             )
             future = self.work_manager.submit(wm_ops.propagate, args=(pbstates, pistates, segment_block))
-            futures.add(future)
             segment_futures.add(future)
 
         self.rc.pstatus('Waiting for segments to complete...')
 
-        while futures:
+        while segment_futures:
             # TODO: add capacity for timeout or SIGINT here
-            future = self.work_manager.wait_any(futures)
-            futures.remove(future)
+            future = self.work_manager.wait_any(segment_futures)
+            segment_futures.remove(future)
 
-            if future in segment_futures:
-                segment_futures.remove(future)
-                incoming = future.get_result()
-                self.n_propagated += 1
+            incoming = future.get_result()
+            self.n_propagated += 1
 
-                self.segments.update({segment.seg_id: segment for segment in incoming})
-                self.completed_segments.update({segment.seg_id: segment for segment in incoming})
+            self.segments.update({segment.seg_id: segment for segment in incoming})
+            self.completed_segments.update({segment.seg_id: segment for segment in incoming})
 
-                new_istate_futures = self.get_istate_futures()
-                istate_gen_futures.update(new_istate_futures)
-                futures.update(new_istate_futures)
+            with self.data_manager.expiring_flushing_lock():
+                self.data_manager.update_segments(self.n_iter, incoming)
 
-                with self.data_manager.expiring_flushing_lock():
-                    self.data_manager.update_segments(self.n_iter, incoming)
-
-            elif future in istate_gen_futures:
-                istate_gen_futures.remove(future)
-                _basis_state, initial_state = future.get_result()
-                log.debug('received newly-prepared initial state {!r}'.format(initial_state))
-                initial_state.istate_status = InitialState.ISTATE_STATUS_PREPARED
-                with self.data_manager.expiring_flushing_lock():
-                    self.data_manager.update_initial_states([initial_state], n_iter=self.n_iter + 1)
-                self.we_driver.avail_initial_states[initial_state.state_id] = initial_state
-            else:
-                log.error('unknown future {!r} received from work manager'.format(future))
-                raise AssertionError('untracked future {!r}'.format(future))
-
-        # Assign bins only when all segments have completed. This is necessary to
+        # Assign bins. This is done only after all segments have completed to
         # accommodate "global" binning schemes (e.g., MAB) that require all the
         # segments at the same time.
         self.we_driver.assign(self.segments.values())
 
+        # Construct initial states. This must be done after assigning bins, since
+        # `we_driver.n_istates_needed` depends on `we_driver.final_binning`.
         istate_gen_futures = self.get_istate_futures()
-        if any(istate_gen_futures):
-            deque(self.work_manager.as_completed(istate_gen_futures), maxlen=0)  # wait on results
+        while istate_gen_futures:
+            future = self.work_manager.wait_any(istate_gen_futures)
+            istate_gen_futures.remove(future)
+
+            _basis_state, initial_state = future.get_result()
+            log.debug('received newly-prepared initial state {!r}'.format(initial_state))
+            initial_state.istate_status = InitialState.ISTATE_STATUS_PREPARED
+
+            with self.data_manager.expiring_flushing_lock():
+                self.data_manager.update_initial_states([initial_state], n_iter=self.n_iter + 1)
+
+            self.we_driver.avail_initial_states[initial_state.state_id] = initial_state
 
         log.debug('done with propagation')
         self.save_bin_data()
