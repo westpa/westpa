e897ffe1305b709292f6b40f8a28d09f20e7e5aa
Jeremy Leung <jml230@pitt.edu>
2023-07-05 11:28:17 -0400

    lint for test_loaders

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,14 +1,9 @@
-import os
-from unittest import mock
-from .test_tools.conftest import *
-from westpa.cli.tools.w_multi_west import entry_point, create_idtype_array
+from .test_tools.conftest import *  # noqa
 from westpa.core.propagators.executable import npy_data_loader, pickle_data_loader
 from westpa.core.segment import Segment
 
-import argparse
 import numpy as np
 import pickle
-import h5py
 
 
 class Test_Loaders:
@@ -17,10 +12,9 @@ class Test_Loaders:
     def test_npy_loader(self, ref_idtype):
         '''Test if data loaded with npy_data_loader is consistent.'''
 
-        input_array = h5py.File(self.h5_filepath)['ibstates/0/istate_index'][:]
         test_segment = Segment()
 
-        npy_data_loader('test', self.correct_pkl, test_segment, False) 
+        npy_data_loader('test', self.correct_pkl, test_segment, False)
 
         with open(self.correct_pkl, 'rb') as f:
             ref_array = pickle.load(f)
@@ -29,14 +23,12 @@ class Test_Loaders:
 
         assert np.array_equal(test_array, ref_array)
 
-
     def test_pickle_loader(self, ref_idtype):
         '''Test if data loaded with npy_data_loader is consistent.'''
 
-        input_array = h5py.File(self.h5_filepath)['ibstates/0/istate_index'][:]
         test_segment = Segment()
 
-        pickle_data_loader('test', self.correct_pkl, test_segment, False) 
+        pickle_data_loader('test', self.correct_pkl, test_segment, False)
 
         with open(self.correct_pkl, 'rb') as f:
             ref_array = pickle.load(f)
