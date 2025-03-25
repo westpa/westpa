eea7a838131667250bc309e01dba5efd043d7f3a
Jeff Thompson <jeffpthompson@proton.me>
2025-03-25 11:06:53 -0600

    Revise test_sink.py.

--- tests/test_sink.py
+++ tests/test_sink.py
@@ -7,15 +7,15 @@ def test_from_string():
     with pytest.raises(ValueError, match="invalid syntax: expected '<variables> : <predicate>'"):
         Sink.from_string('x')
     with pytest.raises(ValueError, match="array variable 'x' must be subscripted"):
-        Sink.from_string('x: x > 0')
+        Sink.from_string('x : x > 0')
     with pytest.raises(ValueError, match="unpacked variable 'x' may not be subscripted"):
-        Sink.from_string('(x, y): x[0] > 0')
+        Sink.from_string('(x, y) : x[0] > 0')
     with pytest.raises(ValueError, match='invalid comparison: x == 0'):
-        Sink.from_string('(x, y): x == 0 and y > 0')
+        Sink.from_string('(x, y) : x == 0 and y > 0')
 
 
 def test_array_variable():
-    sink = Sink.from_string('x: x[0]**2 + x[1]**2 < 1 and x[0] > 0')
+    sink = Sink.from_string('x : x[0]**2 + x[1]**2 < 1 and x[0] > 0')
     assert (0.5, 0.5) in sink
     assert (-0.5, 0.5) not in sink
     assert (1.0, 1.0) not in sink
@@ -28,7 +28,7 @@ def test_array_variable():
 
 
 def test_unpacked_variables():
-    sink = Sink.from_string('(x, y): x**2 + y**2 < 1 and x > 0')
+    sink = Sink.from_string('(x, y) : x**2 + y**2 < 1 and x > 0')
     assert (0.5, 0.5) in sink
     assert (-0.5, 0.5) not in sink
     assert (1.0, 1.0) not in sink
