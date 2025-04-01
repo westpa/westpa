d10a11fc2aae1570d33149f9b2d18a9f77652f62
Jeremy M. G. Leung <jml230@pitt.edu>
2025-03-31 21:24:45 -0400

    fix F824

--- src/westpa/cli/tools/w_ipa.py
+++ src/westpa/cli/tools/w_ipa.py
@@ -78,7 +78,7 @@ class WIPI(WESTParallelTool):
         # Well, whatever, we'll just set it to that for now.
         self.interface = 'matplotlib'
         self._scheme = None
-        global iteration
+        global iteration  # noqa: F824
 
     def add_args(self, parser):
         self.progress.add_args(parser)
