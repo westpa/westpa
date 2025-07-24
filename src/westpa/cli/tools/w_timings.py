1a85b1c67cb2b2a7d7a7f6f0828f1ac2397c63eb
Jeff Thompson <jeffpthompson@proton.me>
2025-07-24 17:51:21 -0600

    Remove w_timings --count-events option (scope overlaps with w_succ).

--- src/westpa/cli/tools/w_timings.py
+++ src/westpa/cli/tools/w_timings.py
@@ -1,6 +1,3 @@
-import numpy as np
-from tqdm.auto import tqdm
-
 from westpa.tools import (
     WESTTool,
     WESTDataReader,
@@ -14,7 +11,6 @@ class WTimings(WESTTool):
 
     TODO:
         * convert to use logging?
-        * convert from tqdm to native westpa progress indicator?
         * write a simple test: this should be very straitforward (assert time of test h5 file is as expected)
         * update docs
     """
@@ -22,14 +18,13 @@ class WTimings(WESTTool):
     prog = "w_timings"
     description = "A tool for aggregate simulation and wallclock time extraction."
 
-    def __init__(self, tau=100, count_events=False):
+    def __init__(self, tau=100):
         """
         Parameters
         ----------
         tau : int
             WESTPA dynamics propagation time in picoseconds. Default 100 = 100ps.
-        count_events : bool
-            Option to also output the number of successfull recycling events.
+
         """
         super().__init__()
         self.data_reader = WESTDataReader()
@@ -37,31 +32,6 @@ class WTimings(WESTTool):
         self.iter_start = None
         self.iter_stop = None
         self.tau = tau
-        self.count_events = count_events
-
-    def get_event_count(self, we_h5file):
-        """
-        Check if the target state was reached, given the data in a WEST H5 file.
-
-        Parameters
-        ----------
-        we_h5file : h5py.File
-
-        Returns
-        -------
-        int
-            Number of successful recycling events.
-        """
-        events = 0
-        # Get the key to the final iteration.
-        # Need to do -2 instead of -1 because there's an empty-ish final iteration written.
-        for iteration_key in tqdm(list(we_h5file['iterations'].keys())[-2:0:-1]):
-            endpoint_types = we_h5file[f'iterations/{iteration_key}/seg_index']['endpoint_type']
-            if 3 in endpoint_types:
-                # print(f"recycled segment found in file {h5_filename} at iteration {iteration_key}")
-                # count the number of 3s
-                events += np.count_nonzero(endpoint_types == 3)
-        return events
 
     def go(self):
         with self.data_reader:
@@ -85,11 +55,6 @@ class WTimings(WESTTool):
             print(f"{'Total Segments:':30} {aggtime}")
             print(f"{'Simulation time:':30} {((aggtime * self.tau) / 1000):.2f} ns")
 
-            if self.count_events:
-                events = self.get_event_count(we_h5file)
-                print("\n===== RECYCLING =====")
-                print(f"{'Recycled walkers:':30} {events}")
-
     def add_args(self, parser):
         self.data_reader.add_args(parser)
         self.iter_range.add_args(parser)
@@ -101,19 +66,10 @@ class WTimings(WESTTool):
             default=100,
             help="WESTPA dynamics propagation time in picoseconds. Default 100 = 100ps.",
         )
-        parser.add_argument(
-            "--count-events",
-            "-ce",
-            dest="count_events",
-            action="store_true",
-            default=False,
-            help="Include this flag to also output the number of successfull recycling events.",
-        )
 
     def process_args(self, args):
         self.data_reader.process_args(args)
         self.tau = args.tau
-        self.count_events = args.count_events
         with self.data_reader:
             self.iter_range.process_args(args)
 
