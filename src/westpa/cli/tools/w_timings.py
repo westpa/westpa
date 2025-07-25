3cdc1f8f80d772c3d5c221e0c4007519a99c8b38
Jeff Thompson <jeffpthompson@proton.me>
2025-07-25 01:42:18 -0600

    Allow passing units for w_timings --tau.

--- src/westpa/cli/tools/w_timings.py
+++ src/westpa/cli/tools/w_timings.py
@@ -1,5 +1,6 @@
-import math
 from datetime import timedelta
+
+import numpy as np
 
 from westpa.tools import (
     WESTTool,
@@ -8,31 +9,39 @@ from westpa.tools import (
 )
 
 
+TIME_UNITS = ['as', 'fs', 'ps', 'ns', 'us', 'ms', 's', 'm', 'h']
+
+
+def _unit(delta):
+    words = str(delta.dtype).split('[')
+    if len(words) == 1:
+        return None
+    return words[1][:-1]
+
+
+def _str(delta):
+    unit = _unit(delta)
+    if unit is None:
+        return str(delta)
+    for new_unit in TIME_UNITS[TIME_UNITS.index(unit) + 1 :]:
+        if delta < np.timedelta64(1, new_unit):
+            break
+        unit = new_unit
+        if delta % np.timedelta64(1, unit):
+            return f'{delta / np.timedelta64(1, unit)} {unit}'
+        delta = delta.astype(f'timedelta64[{unit}]')
+    return f'{delta.astype(int)} {unit}'
+
+
 class WTimings(WESTTool):
-    """
-    Aggregate simulation and wallclock time extraction.
+    prog = 'w_timings'
+    description = 'Print timing information for a WESTPA simulation.'
 
-    TODO:
-        * convert to use logging?
-        * write a simple test: this should be very straitforward (assert time of test h5 file is as expected)
-        * update docs
-    """
-
-    prog = "w_timings"
-    description = "A tool for aggregate simulation and wallclock time extraction."
-
-    def __init__(self, tau=100):
-        """
-        Parameters
-        ----------
-        tau : int
-            WESTPA dynamics propagation time in picoseconds. Default 100 = 100ps.
-
-        """
+    def __init__(self):
         super().__init__()
         self.data_reader = WESTDataReader()
         self.iter_range = IterRangeSelection(self.data_reader)
-        self.tau = tau
+        self.tau = None
 
     def go(self):
         start = self.iter_range.iter_start - 1
@@ -42,34 +51,37 @@ class WTimings(WESTTool):
 
         walltime = iter_summaries['walltime'].sum()
         cputime = iter_summaries['cputime'].sum()
-        aggtime = iter_summaries['n_particles'].sum()
+        n_particles = iter_summaries['n_particles'].sum()
+        n_iters = len(iter_summaries)
 
-        print(f'Total wall-clock time: {timedelta(seconds=walltime)}')
-        if not math.isclose(cputime, 0):  # Only print CPU time if it was recorded.
+        print(f'Iterations: {n_iters}')
+        print(f'Total segments: {n_particles}')
+        print(f'Wall-clock time: {timedelta(seconds=walltime)}')
+        if not np.isclose(cputime, 0):  # only print CPU time if it was recorded
             print(f'Total CPU time: {timedelta(seconds=cputime)}')
-
-        print("\n===== SIMULATION  =====")
-        print(f"{'Tau:':30} {self.tau} ps")
-        print(f"{'Total Segments:':30} {aggtime}")
-        print(f"{'Simulation time:':30} {((aggtime * self.tau) / 1000):.2f} ns")
+        if self.tau is not None:
+            print(f'Simulated physical time ("molecular time"): {_str(n_iters * self.tau)}')
+            print(f'Aggregate simulation time: {_str(n_particles * self.tau)}')
 
     def add_args(self, parser):
         self.data_reader.add_args(parser)
         self.iter_range.add_args(parser)
         parser.add_argument(
-            "--tau",
-            "-t",
-            dest="tau",
-            type=int,
-            default=100,
-            help="WESTPA dynamics propagation time in picoseconds. Default 100 = 100ps.",
+            '-t',
+            '--tau',
+            help=(
+                'WE resampling interval (format: <value>_<unit>, where <value> '
+                'is a positive integer and <unit> is a NumPy time unit code).'
+            ),
         )
 
     def process_args(self, args):
         self.data_reader.process_args(args)
-        self.tau = args.tau
         with self.data_reader:
             self.iter_range.process_args(args)
+        if args.tau is not None:
+            value, *unit = args.tau.split('_')
+            self.tau = np.timedelta64(int(value), *unit)
 
 
 def entry_point():
