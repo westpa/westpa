e9816f2f5f11d4f0d2d78c1f3a134f0b4140cf08
Jeff Thompson <jeffpthompson@proton.me>
2025-07-27 18:11:51 -0600

    Update w_timings tests.

--- tests/test_tools/test_w_timings.py
+++ tests/test_tools/test_w_timings.py
@@ -1,44 +1,62 @@
-import os
 from unittest import mock
 import argparse
-import pytest
+
 from westpa.cli.tools.w_timings import entry_point
 
 
 class Test_W_Timings:
     """Test class for w_timings tool."""
 
-    @pytest.fixture
-    def ref_west_file(self):
-        return "tests/refs/west_3iter.h5"
+    def args(self, first_iter=None, last_iter=None, tau=None):
+        return argparse.Namespace(
+            verbosity=None,
+            rcfile=None,
+            we_h5filename=self.h5_filepath,
+            first_iter=first_iter,
+            last_iter=last_iter,
+            tau=tau,
+        )
 
-    def test_run_w_timings(self, ref_west_file, capsys):
-        """
-        Currently only checks if sections headers are printing correctly
-        """
-
+    def test_default(self, ref_50iter, capsys):
         with mock.patch(
             "argparse.ArgumentParser.parse_args",
-            return_value=argparse.Namespace(
-                verbosity="debug",
-                rcfile=None,
-                we_h5filename=ref_west_file,
-                first_iter=None,
-                last_iter=None,
-                tau=100,
-                count_events=True,
-            ),
+            return_value=self.args(),
         ):
             entry_point()
-
         captured = capsys.readouterr()
         output = captured.out
+        assert output == """\
+Iterations: 50
+Total segments: 9985
+Wall-clock time: 0:00:01.452625
+"""
 
-        # Basic checks that output sections are present
-        # TODO: Replace with actual known values from reference
-        assert "===== WALLCLOCK  =====" in output
-        assert "Total Wallclock Time:" in output
-        assert "===== SIMULATION  =====" in output
-        assert "Simulation time:" in output
-        assert "===== RECYCLING =====" in output
-        assert "Recycled walkers:" in output
+    def test_tau(self, ref_50iter, capsys):
+        with mock.patch(
+            "argparse.ArgumentParser.parse_args",
+            return_value=self.args(tau='100_ps'),
+        ):
+            entry_point()
+        captured = capsys.readouterr()
+        output = captured.out
+        assert output == """\
+Iterations: 50
+Total segments: 9985
+Wall-clock time: 0:00:01.452625
+Maximum trajectory length: 5.0 ns
+Aggregate simulation time: 998.5 ns
+"""
+
+    def test_iter_range(self, ref_50iter, capsys):
+        with mock.patch(
+                "argparse.ArgumentParser.parse_args",
+                return_value=self.args(first_iter=11, last_iter=40),
+        ):
+            entry_point()
+        captured = capsys.readouterr()
+        output = captured.out
+        assert output == """\
+Iterations: 30
+Total segments: 6320
+Wall-clock time: 0:00:00.868997
+"""
