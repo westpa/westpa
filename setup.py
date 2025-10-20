b1348db8c220d2c8ea7ea332c25c4ae89a0ecb69
Jeremy M. G. Leung <jml230@pitt.edu>
2025-10-20 15:17:34 -0400

    move a little back

--- setup.py
+++ setup.py
@@ -61,10 +61,73 @@ def extensions():
     return exts
 
 
+console_scripts_core = [
+    'w_fork = westpa.cli.core.w_fork:entry_point',
+    'w_states = westpa.cli.core.w_states:entry_point',
+    'w_run = westpa.cli.core.w_run:entry_point',
+    'w_truncate = westpa.cli.core.w_truncate:entry_point',
+    'w_init = westpa.cli.core.w_init:entry_point',
+    'w_succ = westpa.cli.core.w_succ:entry_point',
+]
+
+console_scripts_tools = [
+    'w_direct = westpa.cli.tools.w_direct:entry_point',
+    'w_dumpsegs = westpa.cli.tools.w_dumpsegs:entry_point',
+    'w_postanalysis_matrix = westpa.cli.tools.w_postanalysis_matrix:entry_point',
+    'w_select = westpa.cli.tools.w_select:entry_point',
+    'w_ntop = westpa.cli.tools.w_ntop:entry_point',
+    'w_kinavg = westpa.cli.tools.w_kinavg:entry_point',
+    'w_eddist = westpa.cli.tools.w_eddist:entry_point',
+    'w_assign = westpa.cli.tools.w_assign:entry_point',
+    'w_trace = westpa.cli.tools.w_trace:entry_point',
+    'w_crawl = westpa.cli.tools.w_crawl:entry_point',
+    'w_kinetics = westpa.cli.tools.w_kinetics:entry_point',
+    'w_fluxanl = westpa.cli.tools.w_fluxanl:entry_point',
+    'w_reweight = westpa.cli.tools.w_reweight:entry_point',
+    'w_pdist = westpa.cli.tools.w_pdist:entry_point',
+    'w_ipa = westpa.cli.tools.w_ipa:entry_point',
+    'w_bins = westpa.cli.tools.w_bins:entry_point',
+    'w_stateprobs = westpa.cli.tools.w_stateprobs:entry_point',
+    'w_postanalysis_reweight = westpa.cli.tools.w_postanalysis_reweight:entry_point',
+    'ploterr = westpa.cli.tools.ploterr:entry_point',
+    'plothist = westpa.cli.tools.plothist:entry_point',
+    'w_multi_west = westpa.cli.tools.w_multi_west:entry_point',
+    'w_red = westpa.cli.tools.w_red:entry_point',
+    'w_timings = westpa.cli.tools.w_timings:entry_point',
+]
+
+console_scripts = console_scripts_core + console_scripts_tools
+
+INSTALL_REQUIRES = [
+    "numpy >= 1.25.0, <3",
+    "scipy >= 0.19.1",
+    "h5py >= 2.10",
+    "mdtraj >= 1.9.5",
+    "pyyaml",
+    "pyzmq",
+    "matplotlib",
+    "blessings",
+    "ipykernel",
+    "tqdm",
+    "pandas",
+    "tables",
+]
+
+EXTRAS_REQUIRE = {
+    "tests": ["pytest", "pytest-cov", "pytest-rerunfailures", "pytest-timeout"],
+    "mpi": ["mpi4py"],
+}
+
+EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + ["pre-commit"]
+
+
 metadata = dict(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
+    entry_points={'console_scripts': console_scripts},
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
 )
 
 
