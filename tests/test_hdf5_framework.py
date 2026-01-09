6b08032adbd04764af7844275b609086a8b34907
Jeremy M. G. Leung <jml230@pitt.edu>
2026-01-09 14:21:22 -0500

    hide element information warning from mdanalysis tests

--- tests/test_hdf5_framework.py
+++ tests/test_hdf5_framework.py
@@ -12,6 +12,7 @@ from westpa.core.propagators.loaders import mdtraj_trajectory_loader, mdanalysis
 class TestHDF5Framework:
     '''Class to test HDF5 Framework'''
 
+    @pytest.mark.filterwarnings("ignore:Element information is missing")
     def test_load_mdanalysis(self, traj_setup):
         pytest.importorskip('MDAnalysis')
 
@@ -39,6 +40,7 @@ class TestHDF5Framework:
         assert_allclose(test_traj.xyz, self.ref_coords)
         assert_allclose(test_traj.time, self.ref_time)
 
+    @pytest.mark.filterwarnings("ignore:Element information is missing")
     def test_mdanalysis_trajectory_loader(self, traj_setup, monkeypatch):
         pytest.importorskip('MDAnalysis')
 
@@ -50,7 +52,7 @@ class TestHDF5Framework:
         assert_allclose(dummy_segment.data['iterh5/trajectory'].time, self.ref_time)
 
     def test_mdanalysis_trajectory_loader_fail_import(self, traj_setup, monkeypatch):
-        '''Test fallback to MDTraj with `mdanalysis_trajectory_loader`'''
+        """Test fallback to MDTraj with `mdanalysis_trajectory_loader`"""
         dummy_segment = Segment()
 
         with monkeypatch.context() as m:
