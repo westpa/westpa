f5d7d40e5b19b2104a42afc64053994b8b10120e
Jeremy M. G. Leung <jml230@pitt.edu>
2026-01-13 12:15:57 -0500

    new binningfe tests

--- tests/test_binningfe.py
+++ tests/test_binningfe.py
@@ -1,6 +1,9 @@
+import pytest
+
 import numpy as np
 
-from westpa.tools.binning import mapper_from_expr
+from westpa.tools.binning import mapper_from_expr, mapper_from_system
+from westpa.core.binning.assign import RecursiveBinMapper
 
 
 class TestBinParsing:
@@ -11,3 +14,13 @@ class TestBinParsing:
         assert mapper.nbins == 3
         assert np.array_equal(mapper._boundaries, [np.array([-float('inf'), 0, 15, float('inf')], dtype=np.float32)])
         assert mapper.labels == ['[(-inf, 0.0)]', '[(0.0, 15.0)]', '[(15.0, inf)]']
+
+    def test_mapper_from_expr_exceptions(self):
+        with pytest.raises(NameError):
+            mapper_from_expr('abc')
+
+        with pytest.raises(TypeError):
+            mapper_from_expr(int(3))
+
+    def test_mapper_from_system(self, ref_mab):
+        assert isinstance(mapper_from_system(), RecursiveBinMapper)
