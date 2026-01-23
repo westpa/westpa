83d0a16f22dede22d13d6719b63af346d8e77e6c
Jeremy M. G. Leung <jml230@pitt.edu>
2026-01-23 14:41:18 -0500

    refactor tests_analysis.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ import numpy as np
 from scipy.io import netcdf_file
 
 import westpa
-from westpa.analysis import Run
 from westpa.core.h5io import WESTIterationFile
 
 
@@ -303,20 +302,3 @@ def nacl_restart_files(request, tmp_path):
 
     for file in request.cls.nacl_restart_files:
         copyfile(os.path.join(REFERENCE_PATH, file), request.cls.return_dir / file)
-
-
-@pytest.fixture(scope='class')
-def ref_analysis(request, tmp_path_factory):
-    """
-    Fixture that prepares a simulation directory with a completed 50-iteration WESTPA,
-    west.h5, plus the config file west.cfg, plus a westpa.analysis.Run object.
-    """
-    test_dir = str(tmp_path_factory.getbasetemp())
-
-    os.chdir(test_dir)
-    copy_ref(test_dir)
-
-    copyfile(os.path.join(REFERENCE_PATH, 'west_ref.h5'), H5_FILENAME)
-    request.cls.h5_filepath = H5_FILENAME
-
-    request.cls.run = Run(H5_FILENAME)
--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,8 +1,10 @@
 import itertools
+from pathlib import Path
 
 import h5py
 import numpy as np
 import pandas as pd
+import pytest
 
 from westpa.analysis import Run
 from westpa.analysis.core import Iteration, Walker
@@ -12,165 +14,176 @@ from westpa.core.states import BasisState, InitialState, TargetState
 
 
 class TestAnalysis:
-    def test_open_close(self, ref_analysis):
-        run = Run.open(self.h5_filepath)
-        assert not run.closed
+    @pytest.fixture
+    def h5filename(self) -> str:
+        p = Path(__file__).parents[0] / 'refs' / 'west_ref.h5'
+        return str(p)
+
+    @pytest.fixture
+    def run(self, h5filename) -> Run:
+        run = Run.open(h5filename)
+        yield run
         run.close()
-        assert run.closed
 
-    def test_context_manager(self, ref_analysis):
-        with Run.open(self.h5_filepath) as run:
-            assert not run.closed
-        assert run.closed
+    def test_open_close(self, h5filename):
+        run_obj = Run.open(h5filename)
+        assert not run_obj.closed
+        run_obj.close()
+        assert run_obj.closed
 
-    def test_num_iterations(self, ref_analysis):
-        assert self.run.num_iterations == 50
-        assert len(self.run) == self.run.num_iterations  # Run.__len__
+    def test_context_manager(self, h5filename):
+        with Run.open(h5filename) as run_obj:
+            assert not run_obj.closed
+        assert run_obj.closed
 
-    def test_iteration(self, ref_analysis):
-        for n in range(1, self.run.num_iterations + 1):
-            iteration = self.run.iteration(n)
+    def test_num_iterations(self, run):
+        assert run.num_iterations == 50
+        assert len(run) == run.num_iterations  # Run.__len__
+
+    def test_iteration(self, run):
+        for n in range(1, run.num_iterations + 1):
+            iteration = run.iteration(n)
             assert isinstance(iteration, Iteration)
-            assert iteration in self.run
-            assert iteration.run is self.run
+            assert iteration in run
+            assert iteration.run is run
             assert iteration.number == n
 
-    def test_iterations(self, ref_analysis):
-        assert isinstance(self.run.iterations, list)
-        assert self.run.iterations == list(self.run)  # Run.__iter__, Iteration.__eq__
-        for iteration, n in zip(self.run.iterations, range(1, self.run.num_iterations + 1)):
+    def test_iterations(self, run):
+        assert isinstance(run.iterations, list)
+        assert run.iterations == list(run)  # Run.__iter__, Iteration.__eq__
+        for iteration, n in zip(run.iterations, range(1, run.num_iterations + 1)):
             assert isinstance(iteration, Iteration)
-            assert iteration in self.run
-            assert iteration.run is self.run
+            assert iteration in run
+            assert iteration.run is run
             assert iteration.number == n
 
-    def test_num_walkers(self, ref_analysis):
-        assert self.run.num_walkers == 9985
-        assert sum(iteration.num_walkers for iteration in self.run) == self.run.num_walkers
-        assert self.run.num_walkers == self.run.num_segments  # alias
-        for iteration in self.run:
+    def test_num_walkers(self, run):
+        assert run.num_walkers == 9985
+        assert sum(iteration.num_walkers for iteration in run) == run.num_walkers
+        assert run.num_walkers == run.num_segments  # alias
+        for iteration in run:
             assert iteration.num_walkers == iteration.num_segments  # alias
 
-    def test_walker(self, ref_analysis):
-        iteration = self.run.iteration(1)
+    def test_walker(self, run):
+        iteration = run.iteration(1)
         for i in range(iteration.num_walkers):
             walker = iteration.walker(i)
             assert isinstance(walker, Walker)
-            assert walker in self.run
+            assert walker in run
             assert walker in iteration
-            assert walker.run is self.run
+            assert walker.run is run
             assert walker.iteration is iteration
             assert walker.index == i
 
-    def test_walkers(self, ref_analysis):
-        for walker in self.run.walkers:
+    def test_walkers(self, run):
+        for walker in run.walkers:
             assert isinstance(walker, Walker)
-            assert walker in self.run
-            assert walker.run is self.run
+            assert walker in run
+            assert walker.run is run
 
-        assert len(list(self.run.walkers)) == self.run.num_walkers
+        assert len(list(run.walkers)) == run.num_walkers
 
-        for iteration in self.run:
+        for iteration in run:
             assert list(iteration.walkers) == list(iteration)  # Iteration.__iter__, Walker.__eq__
             for i, walker in enumerate(iteration.walkers):
                 assert isinstance(walker, Walker)
-                assert walker in self.run
+                assert walker in run
                 assert walker in iteration
-                assert walker.run is self.run
+                assert walker.run is run
                 assert walker.iteration is iteration
                 assert walker.index == i
-        assert sum(len(list(iteration.walkers)) for iteration in self.run) == self.run.num_walkers
+        assert sum(len(list(iteration.walkers)) for iteration in run) == run.num_walkers
 
-    def test_parent(self, ref_analysis):
-        for walker in self.run.iteration(1):
+    def test_parent(self, run):
+        for walker in run.iteration(1):
             assert isinstance(walker.parent, InitialState)
-        for walker in self.run.iteration(2):
+        for walker in run.iteration(2):
             assert isinstance(walker.parent, Walker)
             assert walker.parent.iteration.number == 1
 
-    def test_children(self, ref_analysis):
-        for walker in self.run.iteration(1):
+    def test_children(self, run):
+        for walker in run.iteration(1):
             for child in walker.children:
                 assert isinstance(child, Walker)
                 assert child.iteration.number == 2
 
-    def test_recycled(self, ref_analysis):
-        for walker in self.run.iteration(1):
+    def test_recycled(self, run):
+        for walker in run.iteration(1):
             assert not walker.recycled
 
-    def test_initial(self, ref_analysis):
-        for walker in self.run.iteration(1):
+    def test_initial(self, run):
+        for walker in run.iteration(1):
             assert walker.initial
 
-    def test_recycled_walkers(self, ref_analysis):
-        for walker in self.run.recycled_walkers:
+    def test_recycled_walkers(self, run):
+        for walker in run.recycled_walkers:
             assert walker.recycled
-        assert len(list(self.run.recycled_walkers)) == 39
+        assert len(list(run.recycled_walkers)) == 39
         for walker1, walker2 in zip(
-            self.run.recycled_walkers,
-            itertools.chain(*(iteration.recycled_walkers for iteration in self.run)),
+            run.recycled_walkers,
+            itertools.chain(*(iteration.recycled_walkers for iteration in run)),
         ):
             assert walker1 == walker2
 
-    def test_initial_walkers(self, ref_analysis):
-        for walker in self.run.initial_walkers:
+    def test_initial_walkers(self, run):
+        for walker in run.initial_walkers:
             assert walker.initial
             assert isinstance(walker.parent, InitialState)
-        assert len(list(self.run.initial_walkers)) == 5
+        assert len(list(run.initial_walkers)) == 5
         for walker1, walker2 in zip(
-            self.run.initial_walkers,
-            itertools.chain(*(iteration.initial_walkers for iteration in self.run)),
+            run.initial_walkers,
+            itertools.chain(*(iteration.initial_walkers for iteration in run)),
         ):
             assert walker1 == walker2
 
-    def test_auxiliary_data(self, ref_analysis):
-        for iteration in self.run:
+    def test_auxiliary_data(self, run):
+        for iteration in run:
             assert iteration.auxiliary_data is None
-        for walker in self.run.iteration(1):
+        for walker in run.iteration(1):
             assert walker.auxiliary_data == {}
 
-    def test_basis_state_summaries(self, ref_analysis):
-        for iteration in self.run:
+    def test_basis_state_summaries(self, run):
+        for iteration in run:
             summaries = iteration.basis_state_summaries
             assert isinstance(summaries, pd.DataFrame)
             assert list(summaries.axes[1]) == ['label', 'probability', 'auxref']
 
-    def test_basis_state_pcoords(self, ref_analysis):
-        for iteration in self.run:
+    def test_basis_state_pcoords(self, run):
+        for iteration in run:
             assert iteration.basis_state_pcoords.ndim == 2
 
-    def test_basis_states(self, ref_analysis):
-        for iteration in self.run:
+    def test_basis_states(self, run):
+        for iteration in run:
             basis_states = iteration.basis_states
             assert isinstance(basis_states, list)
             assert all(isinstance(state, BasisState) for state in basis_states)
 
-    def test_has_target_states(self, ref_analysis):
-        for iteration in self.run:
+    def test_has_target_states(self, run):
+        for iteration in run:
             assert iteration.has_target_states
 
-    def test_target_state_summaries(self, ref_analysis):
-        for iteration in self.run:
+    def test_target_state_summaries(self, run):
+        for iteration in run:
             summaries = iteration.target_state_summaries
             assert isinstance(summaries, pd.DataFrame)
             assert list(summaries.axes[1]) == ['label']
 
-    def test_target_state_pcoords(self, ref_analysis):
-        for iteration in self.run:
+    def test_target_state_pcoords(self, run):
+        for iteration in run:
             assert iteration.target_state_pcoords.ndim == 2
 
-    def test_target_states(self, ref_analysis):
-        for iteration in self.run:
+    def test_target_states(self, run):
+        for iteration in run:
             target_states = iteration.target_states
             assert isinstance(target_states, list)
             assert all(isinstance(state, TargetState) for state in target_states)
 
-    def test_sink(self, ref_analysis):
-        for walker in self.run.recycled_walkers:
+    def test_sink(self, run):
+        for walker in run.recycled_walkers:
             assert walker.pcoords[0] not in walker.iteration.sink
             assert walker.pcoords[-1] in walker.iteration.sink
 
-    def test_summary(self, ref_analysis):
+    def test_summary(self, run):
         fields = [
             'n_particles',
             'min_bin_prob',
@@ -180,18 +193,18 @@ class TestAnalysis:
             'cputime',
             'walltime',
         ]
-        summary = self.run.summary
+        summary = run.summary
         assert isinstance(summary, pd.DataFrame)
-        assert len(summary) == len(self.run)
-        assert all(summary.index == range(1, self.run.num_iterations + 1))
+        assert len(summary) == len(run)
+        assert all(summary.index == range(1, run.num_iterations + 1))
         assert list(summary.axes[1]) == fields
-        for iteration in self.run:
+        for iteration in run:
             summary = iteration.summary
             assert isinstance(summary, pd.Series)
             assert list(summary.index) == fields
             assert summary.name == iteration.number
 
-    def test_segment_summaries(self, ref_analysis):
+    def test_segment_summaries(self, run):
         fields = [
             'weight',
             'parent_id',
@@ -202,52 +215,52 @@ class TestAnalysis:
             'endpoint_type',
             'status',
         ]
-        for iteration in self.run:
+        for iteration in run:
             summaries = iteration.segment_summaries
             assert isinstance(summaries, pd.DataFrame)
             assert len(summaries) == iteration.num_walkers
             assert all(summaries.index == range(iteration.num_walkers))
             assert list(summaries.axes[1]) == fields
-        for walker in self.run.iteration(1):
+        for walker in run.iteration(1):
             summary = walker.segment_summary
             assert isinstance(summary, pd.Series)
             assert list(summary.index) == fields
 
-    def test_h5filename(self, ref_analysis):
-        assert self.run.h5filename == self.h5_filepath
+    def test_h5filename(self, run, h5filename):
+        assert run.h5filename == h5filename
 
-    def test_h5file(self, ref_analysis):
-        assert isinstance(self.run.h5file, WESTPAH5File)
-        assert self.run.h5file.filename == self.run.h5filename
+    def test_h5file(self, run):
+        assert isinstance(run.h5file, WESTPAH5File)
+        assert run.h5file.filename == run.h5filename
 
-    def test_h5group(self, ref_analysis):
-        for iteration in self.run:
+    def test_h5group(self, run):
+        for iteration in run:
             assert isinstance(iteration.h5group, h5py.Group)
-            assert iteration.h5group == self.run.h5file.get_iter_group(iteration.number)
+            assert iteration.h5group == run.h5file.get_iter_group(iteration.number)
 
-    def test_prev_next(self, ref_analysis):
-        for iteration1, iteration2 in zip(self.run.iterations[:-1], self.run.iterations[1:]):
+    def test_prev_next(self, run):
+        for iteration1, iteration2 in zip(run.iterations[:-1], run.iterations[1:]):
             assert iteration1.next == iteration2
             assert iteration2.prev == iteration1
 
-    def test_pcoords(self, ref_analysis):
-        for iteration in self.run:
+    def test_pcoords(self, run):
+        for iteration in run:
             assert iteration.pcoords.ndim == 3
             assert len(iteration.pcoords) == iteration.num_walkers
-        pcoords = self.run.iteration(1).pcoords
-        for walker in self.run.iteration(1):
+        pcoords = run.iteration(1).pcoords
+        for walker in run.iteration(1):
             assert np.allclose(walker.pcoords, pcoords[walker.index])
 
-    def test_weights(self, ref_analysis):
-        for iteration in self.run:
+    def test_weights(self, run):
+        for iteration in run:
             assert iteration.weights.ndim == 1
             assert len(iteration.weights) == iteration.num_walkers
-        weights = self.run.iteration(1).weights
-        for walker in self.run.iteration(1):
+        weights = run.iteration(1).weights
+        for walker in run.iteration(1):
             assert np.isclose(walker.weight, weights[walker.index])
 
-    def test_binning(self, ref_analysis):
-        for iteration in self.run:
+    def test_binning(self, run):
+        for iteration in run:
             bin_mapper = iteration.bin_mapper
             if bin_mapper is None:
                 assert iteration.bin_target_counts is None
@@ -257,8 +270,8 @@ class TestAnalysis:
                 assert len(iteration.bin_target_counts) == bin_mapper.nbins
                 assert iteration.num_bins == bin_mapper.nbins
 
-    def test_trace(self, ref_analysis):
-        walker = self.run.iterations[-1].walker(0)
+    def test_trace(self, run):
+        walker = run.iterations[-1].walker(0)
         trace = walker.trace()
         assert len(trace) == walker.iteration.number
         assert all(isinstance(walker, Walker) for walker in trace)
