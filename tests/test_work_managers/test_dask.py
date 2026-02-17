2ecbcf1d39b7861f04831675367b5a0483353fab
Jeff Thompson <jeffpthompson@proton.me>
2026-02-17 14:09:46 -0700

    Add TestDaskWorkManager class and test timeout.

--- tests/test_work_managers/test_dask.py
+++ tests/test_work_managers/test_dask.py
@@ -6,43 +6,42 @@ NUM_TASKS = 5
 QUEUE_SIZE = 2
 
 
-@pytest.fixture(scope='module')
-def work_manager():
-    return DaskWorkManager()
+@pytest.mark.timeout(5)
+class TestDaskWorkManager:
 
+    @pytest.fixture(scope='class')
+    def work_manager(self):
+        with DaskWorkManager() as work_manager:
+            yield work_manager
 
-def test_submit(work_manager):
-    future = work_manager.submit(sum, args=[(1, 2)])
-    assert not future.done
-    assert future.result == 3
-    assert future.done
+    def test_submit(self, work_manager):
+        future = work_manager.submit(sum, args=[(1, 2)])
+        assert not future.done
+        assert future.result == 3
+        assert future.done
 
+    def test_as_completed(self, work_manager):
+        futures = [work_manager.submit(str, args=[index]) for index in range(NUM_TASKS)]
+        for future in work_manager.as_completed(futures):
+            print(future)
+            assert future.done
+            assert future.result == str(futures.index(future))
 
-def test_as_completed(work_manager):
-    futures = [work_manager.submit(str, args=[index]) for index in range(NUM_TASKS)]
-    for future in work_manager.as_completed(futures):
-        print(future)
+    def test_submit_as_completed(self, work_manager):
+        tasks = [(str, [index], None) for index in range(NUM_TASKS)]
+        for future in work_manager.submit_as_completed(iter(tasks), queue_size=QUEUE_SIZE):
+            assert future.done
+            assert isinstance(future.result, str)
+
+    def test_wait_any(self, work_manager):
+        futures = [work_manager.submit(str, args=[index]) for index in range(NUM_TASKS)]
+        future = work_manager.wait_any(futures)
         assert future.done
         assert future.result == str(futures.index(future))
 
-
-def test_submit_as_completed(work_manager):
-    tasks = [(str, [index], None) for index in range(NUM_TASKS)]
-    for future in work_manager.submit_as_completed(iter(tasks), queue_size=QUEUE_SIZE):
+    def test_exception(self, work_manager):
+        future = work_manager.submit(int, args=['a'])
+        with pytest.raises(ValueError):
+            _ = future.result
+        assert isinstance(future.exception, ValueError)
         assert future.done
-        assert isinstance(future.result, str)
-
-
-def test_wait_any(work_manager):
-    futures = [work_manager.submit(str, args=[index]) for index in range(NUM_TASKS)]
-    future = work_manager.wait_any(futures)
-    assert future.done
-    assert future.result == str(futures.index(future))
-
-
-def test_exception(work_manager):
-    future = work_manager.submit(int, args=['a'])
-    with pytest.raises(ValueError):
-        _ = future.result
-    assert isinstance(future.exception, ValueError)
-    assert future.done
