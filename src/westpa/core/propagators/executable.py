931328b199a2733d78c89196d9c69269f6c5c626
Jeremy M. G. Leung <jml230@pitt.edu>
2026-02-27 14:57:22 -0500

    document why I initialize later

--- src/westpa/core/propagators/executable.py
+++ src/westpa/core/propagators/executable.py
@@ -221,6 +221,8 @@ class ExecutablePropagator(WESTPropagator):
         as a dictionary, suitable for use in ``os.environ.update()`` or as the ``env`` argument to
         ``subprocess.Popen()``. Every child process executed by ``exec_child()`` gets these.'''
 
+        # Initialize rng here when we need our first number. This will prevent
+        # certain work managers (e.g., `processes`) from reusing an initialized RNG.
         if self.rng is None:
             self.rng = Generator(MT19937())
 
