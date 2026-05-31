d7fa8392da1642f91175fff10fced33bf27a0bfe
Edison <77561470+edison16a@users.noreply.github.com>
2026-05-30 21:48:05 -0700

    Track live progress state in the sim manager
    Add WESimManager fields for the status writer and live progress counters so w_run can maintain recent walltimes, completed walltime, completed segments, and current iteration timing for w_progress

--- src/westpa/core/sim_manager.py
+++ src/westpa/core/sim_manager.py
@@ -97,6 +97,7 @@ class WESimManager:
         self.bin_mapper_hash = None  # Hash of bin mapper from most recently-run WE, for use by post-WE analysis plugins
 
         # Live status sidecar used by w_progress while west.h5 is locked by w_run.
+        self.run_status_writer = None
         # Pseudo Random Number Generator
         self.rng = Generator(MT19937())
 
