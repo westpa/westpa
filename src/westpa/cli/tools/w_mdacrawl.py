9e78949f4c85151150367e751731553e262db554
kunjsinha <kunjsinha@users.noreply.github.com>
2026-06-01 18:42:13 +0530

    Add w_mdacrawl.py file to westpa.cli.tools

---
+++ src/westpa/cli/tools/w_mdacrawl.py
@@ -0,0 +1,18 @@
+'''
+TODO: Implement the CLI tool for WESTPAParser and WESTAPAReader.
+This module will eventually contain the entry point and command-line
+interface for w_mdacrawl
+'''
+
+
+def entry_point():
+    '''
+    Placeholder entry point for w_mdacrawler tool.
+    Will be implemented later.
+    '''
+    print('This tool is under development and will be added at a later stage in the project.')
+    pass
+
+
+if __name__ == "__main__":
+    entry_point()
