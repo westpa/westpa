3293953a26ffb097ccb3261d0f6674ea7e78f7e2
Jeremy M. G. Leung <jml230@pitt.edu>
2026-08-28 14:03:40 -0400

    test_reweight.py

---
+++ tests/test_reweight.py
@@ -0,0 +1,9 @@
+import sys
+
+from westpa.core.reweight import reweight_for_c  # noqa:F401
+
+
+def test_reweight_for_c():
+    '''Placeholder import test'''
+
+    assert 'westpa.core.reweight' in sys.modules
