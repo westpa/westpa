3e8a52b2355b5dfea914baf2f15125cec98edc55
Jeremy M. G. Leung <63817169+jeremyleung521@users.noreply.github.com>
2026-09-11 15:18:51 -0400

    add nvc-specific flag to allow compilation (#615)
    * add nvc-specific flag to allow compilation
    
    * use endswith instead

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from setuptools import setup, Extension
@@ -14,6 +15,9 @@ def extensions():
     common_cflags = [
         '-O3',
     ]
+
+    # Flag to prevent nvc from throwing `nvc-Error-Unknown switch` errors
+    common_cflags += ['-noswitcherror'] if os.environ.get("CC", "").endswith('nvc') else []
 
     fasthist_module = Extension(
         'westpa.fasthist._fasthist',
